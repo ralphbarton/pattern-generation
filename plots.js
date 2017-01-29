bf6c94916a5941cb8ab2ba3c76740d6f43368018
Ralph Barton <ralph_barton_@hotmail.com>
2017-01-29 01:55:20 +0000

    Plots: update to divide up the rendering work into time-chunks and make this a "background" task
     - this is achieved by (1) storing all the context information (i.e. x,y counters etc) for interation in a global data object so that work can be returned to (2) recursively chaining timeout calls, causing blocks of work to take place
     - implemented feature to iteratively improve row resolution using random row order (for global effect)
     - brown colour indicates when plotting is in progress (provide more details on this)

--- plots.js
+++ plots.js
@@ -123,63 +123,132 @@ var plots = {
     },
 
 
-    prep: function(){},
 
-    work: function(){},
 
-    exec: function(res){
+    CellSizes: [81, 27, 9, 3, 1],
+    wcx: {
+	compilled_formula: undefined,
+	canvas_ctx: undefined,
+//	samples_sets: [], // DO WE STORE THE CALCULATED VALUES?????????????
+	winW: undefined,
+	winH: undefined,
+	cell_size: undefined,
+	interval_size: undefined,
+	r_aspect: undefined,	
+	n_steps_x: undefined,
+	n_steps_y: undefined,
+	n_steps_xH: undefined,
+	n_steps_yH: undefined,
+	x_randomise: undefined,
+	x: undefined,
+	y: undefined,
+	res: undefined,
+	req_abort: false,
+	running: false
+    },
 
-	var winW = $(window).width();
-	var winH = $(window).height();
-
-	//get or create new canvas for the plot...
-
-	//TODO - use multiple canvases, one for each plot...
-	if($("#plot-canv").length > 0){
-	    var ctx = $("#plot-canv")[0].getContext('2d');
-	    $("#plot-canv").attr("width", winW)
-		.attr("height", winH);
+    draw_job: function(){
+	
+	
+	if(this.wcx.running != false){
+	    this.wcx.req_abort = true;
 
 	}else{
-	    var $pc = $('<canvas/>')
-		.attr("width", winW)
-		.attr("height", winH)
-		.attr("id", "plot-canv");
-	    $("#backgrounds").append($pc);
-	    var ctx = $pc[0].getContext('2d');
+
+	    $("#tabs-4 #z-5 #res-lim").css("background-color", "rgba(147, 90, 9, 0.7)");
+
+	    var Plot_i = DM.PlotsArray[this.selected_row_i];
+	    this.wcx.compilled_formula = math.compile(Plot_i.formula);
+
+	    //get or create new canvas for the plot...
+	    //TODO - use multiple canvases, one for each plot...
+
+	    //sets the global this.wcx.canvas_ctx
+	    this.wcx.winW = $(window).width();
+	    this.wcx.winH = $(window).height();
+
+	    if($("#plot-canv").length > 0){
+		this.wcx.canvas_ctx = $("#plot-canv")[0].getContext('2d');
+		$("#plot-canv").attr("width", this.wcx.winW)
+		    .attr("height", this.wcx.winH);
+
+	    }else{
+		var $pc = $('<canvas/>')
+		    .attr("width", this.wcx.winW)
+		    .attr("height", this.wcx.winH)
+		    .attr("id", "plot-canv");
+		$("#backgrounds").append($pc);
+		this.wcx.canvas_ctx = $pc[0].getContext('2d');
+	    }
+
+	    //function calls
+	    this.set_for_res(0);
+	    // 10000 iterations seems to take around 100ms on my laptop => around 50% duty
+	    this.work(10000, 100);//start the chain
+	}
+    },
+
+    set_for_res: function(res){
+
+	this.wcx.res = res;
+	this.wcx.x = 0;
+	this.wcx.y = 0;
+
+	this.wcx.cell_size = this.CellSizes[this.wcx.res];
+	this.wcx.interval_size = 2 * (this.wcx.cell_size/this.wcx.winW);// in units of [-1, +1] for function
+	this.wcx.r_aspect = this.wcx.winH / this.wcx.winW;
+
+	this.wcx.n_steps_x = Math.ceil((1 / this.wcx.interval_size) - 0.5)*2 + 1;
+	this.wcx.n_steps_y = Math.ceil((this.wcx.r_aspect / this.wcx.interval_size) - 0.5)*2 + 1;
+	this.wcx.n_steps_xH = Math.floor(this.wcx.n_steps_x/2)// number of steps wholely contained in x<0 half
+	this.wcx.n_steps_yH = Math.floor(this.wcx.n_steps_y/2)// number of steps wholely contained in x<0 half
+
+	//logic here to manage random column order feature
+	this.wcx.x_randomise = [];
+
+	for(var i = 0; i < this.wcx.n_steps_x; i++){
+	    this.wcx.x_randomise.push({
+		i: i,
+		marker: Math.random()
+	    });
 	}
 
-	var Plot_i = DM.PlotsArray[this.selected_row_i];
-	var compilled_formula = math.compile(Plot_i.formula);
+	function compare(a,b) {
+	    if (a.marker < b.marker)
+		return -1;
+	    if (a.marker > b.marker)
+		return 1;
+	    return 0;
+	}
 
-	var resolution = [81, 27, 9, 3, 1];// max_i =4
-//	var samples_sets = []; DO WE NEED TO STORE THE CALCULATED VALUES?????????????
-	var cell_size = resolution[res];
-//	var samples = samples_sets[res];
-	var interval_size = 2 * (cell_size/winW);// in units of [-1, +1] for function
-	var r_aspect = winH / winW;
+	this.wcx.x_randomise.sort(compare);
 
-	var n_steps_x = Math.ceil((1 / interval_size) - 0.5)*2 + 1;
-	var n_steps_y = Math.ceil((r_aspect / interval_size) - 0.5)*2 + 1;
-	var n_steps_xH = Math.floor(n_steps_x/2)// number of steps wholely contained in x<0 half
-	var n_steps_yH = Math.floor(n_steps_y/2)// number of steps wholely contained in x<0 half
-//	console.log(winW, winH, n_steps_x, n_steps_y);
+    },
 
-	var x = 0;
 
-	while(x < n_steps_x){		
-//	    samples[x] = [];
-	    y = 0;
-	    while(y < n_steps_y){		
+
+
+
+    work: function(iterations, pause_duration){
+
+	//var t_sta = new Date();
+	var completed = false;
+	if(this.wcx.req_abort){
+	    this.wcx.running = false;
+	    this.wcx.req_abort = false;
+	}
+	else{
+	    for(var i = 0; i<iterations; i++){
 
 		// 1. calculating the sample
-		var x_location = (x - n_steps_xH) * interval_size;
-		var y_location = (y - n_steps_yH) * interval_size;
+		var random_x = this.wcx.x_randomise[this.wcx.x].i;
+		var x_location = (random_x - this.wcx.n_steps_xH) * this.wcx.interval_size;
+		var y_location = (this.wcx.y - this.wcx.n_steps_yH) * this.wcx.interval_size;
 		var my_z = math.complex(x_location, y_location)
-		var my_fz = compilled_formula.eval({z: my_z});
+		var my_fz = this.wcx.compilled_formula.eval({z: my_z});
 		var my_h = my_fz.im;
 
-		//samples[x][y] = my_h; // store calculated value....
+		//samples[random_x][this.wcx.y] = my_h; // store calculated value....
 
 		// 2. convert value into colour
 		var UU = 2;
@@ -188,23 +257,60 @@ var plots = {
 		var color = tinycolor.fromRatio({ h: hue, s: 1, l: 0.5 });
 
 		// 3. Draw onto canvas
-		ctx.fillStyle = "#" + color.toHex();
-		var x_location_px = Math.round((winW/2) + (x - n_steps_xH - 0.5)*cell_size);
-		var y_location_px = Math.round((winH/2) + (y - n_steps_yH - 0.5)*cell_size);
+		this.wcx.canvas_ctx.fillStyle = "#" + color.toHex();
+		var x_location_px = Math.round((this.wcx.winW/2) + (random_x - this.wcx.n_steps_xH - 0.5)*this.wcx.cell_size);
+		var y_location_px = Math.round((this.wcx.winH/2) + (this.wcx.y - this.wcx.n_steps_yH - 0.5)*this.wcx.cell_size);
 
-		ctx.fillRect (x_location_px, y_location_px, cell_size, cell_size);//x,y,w,h
+		this.wcx.canvas_ctx.fillRect (x_location_px, y_location_px, this.wcx.cell_size, this.wcx.cell_size);//x,y,w,h
 		
-		y++;
+		this.wcx.y++;
+
+		if(this.wcx.y >= this.wcx.n_steps_y){//test if column finished
+		    this.wcx.y=0;
+		    this.wcx.x++;
+
+		    if(this.wcx.x >= this.wcx.n_steps_x){//test if screen finished
+			this.wcx.x = 0;
+			this.wcx.res++;
+			var res_lim = parseInt($("#tabs-4 #z-5 #res-lim input").val());
+			if((this.wcx.res>=this.CellSizes.length)||(this.CellSizes[this.wcx.res]<res_lim)){//test full completion
+			    /// terminate and flag no further callbacks
+			    completed = true;
+			    break;
+			}else{
+			    //recalculate a bunch of resolution stuff
+			    this.set_for_res(this.wcx.res);
+			    break; //also, pause for breath after doing so.
+			}		    
+		    }else{//another col...
+			random_x = this.wcx.x_randomise[this.wcx.x].i;	//update
+//			samples[random_x] = [];
+		    }
+		}
 	    }
-	    x++;
+
+	    
+	    if(!completed){
+		setTimeout(function(){
+		    plots.work(iterations, pause_duration);
+		}, pause_duration);
+	    }else{
+		//something to indicate we're finished...
+		$("#tabs-4 #z-5 #res-lim").css("background-color", "transparent");
+	    }
+
 	}
 
-	res++;
-	var res_lim = parseInt($("#tabs-4 #z-5 #res-lim input").val());
-	console.log(res_lim);
-	if((res<5)&&(resolution[res]>=res_lim)){
-	    setTimeout(function(){plots.exec(res);}, 100);
-	}
+//	console.log("duration of work", (new Date() - t_sta));
+    },
+
+
+
+
+    exec: function(res){
+
+	this.draw_job();
+
 
 /*
 
