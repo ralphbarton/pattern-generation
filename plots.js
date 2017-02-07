dd87d68673c6872c296a89f9ebea010ffe729936
Ralph Barton <ralph_barton_@hotmail.com>
2017-02-07 15:53:20 +0000

    Plots: added copy-pasted bar-graph D3 code...

--- plots.js
+++ plots.js
@@ -247,6 +247,40 @@ var plots = {
 	$(".zone#z-2 #hist-stats #high-10 span").text(grab(L*0.9));
 	$(".zone#z-2 #hist-stats #median  span").text(grab(L*0.5));
 
+
+	////maybe split into separate function: D3 histogram drawing here...
+
+	//Width and height
+	var w = 143;
+	var h = 76;
+	var barPadding = 1;
+	
+	var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+			11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+	
+	//Create SVG element
+	var svg = d3.select("#hist svg")
+	    .attr("width", w)
+	    .attr("height", h);
+
+	svg.selectAll("rect")
+	    .data(dataset)
+	    .enter()
+	    .append("rect")
+	    .attr("x", function(d, i) {
+		return i * (w / dataset.length);
+	    })
+	    .attr("y", function(d) {
+		return h - (d * 2);
+	    })
+	    .attr("width", w / dataset.length - barPadding)
+	    .attr("height", function(d) {
+		return d * 2;
+	    })
+	    .attr("fill", function(d) {
+		return "rgb(0, 0, " + (d * 10) + ")";
+	    });
+
     }
 
 };
