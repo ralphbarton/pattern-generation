027f54026e7d786acd1f15086a348c0b370722a4
Ralph Barton <ralph_barton_@hotmail.com>
2017-02-14 21:21:51 +0000

    Hand-drawn examples gallery: descriptive text added.
     - descriptions contained within pure-text file
     - file parsed and a JSON object containing the data is created from it
     - nodejs script written to autogenerate this file

---
+++ filename-processing/parse-descriptive-text.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+
+var path_input_file = "/home/ralph/ln-pat-gen/Decriptive text.txt";
+var path_output_file = "/home/ralph/ln-pat-gen/hand-descriptions.js";
+
+//JavaScript: trim leading or trailing spaces from a string
+function trim (mystring){
+    return mystring.replace(/^\s+|\s+$/g, "");
+}
+
+
+/*
+var desc_text = [
+{
+    pattern_id:
+    completion_date:
+    materials_used:
+    dimentions:
+    img_comments:{
+	"a.jpg": "it's yellow!",
+	"b.jpg": "it's green!",
+    }
+}
+
+];
+*/
+
+var DescriptionsArr = [];
+
+fs.readFile(path_input_file, 'utf8', function (err,data) {
+
+    if (err) {return console.log(err);}
+
+    //code to run when the file has been read into memory
+    var lines = data.split("\n");
+    var n_lines = lines.length;
+    var ix = 0; //counter for the line I've reached in my parsing...
+    
+
+    // skip lines until we find one that starts with an equals character
+    while((lines[ix][0]!="=") && (ix < n_lines)){ix++;}
+    
+    //assume we're at an "=" line
+    while(ix < n_lines){
+
+	var desc_obj = {
+	    pattern_id:       trim(lines[ix].replace("=", "")),
+	    completion_date:  trim(lines[ix+1].replace("A:", "")),
+	    materials_used:   trim(lines[ix+2].replace("B:", "")),
+	    description:      trim(lines[ix+3].replace("C:", "")),
+	    dimentions:       trim(lines[ix+4].replace("D:", "")),
+	    img_comments: {}
+	};
+
+	ix += 5; //we should be now be on the line starting capital E
+	ix ++; //now at the list of individual files and description...
+
+	while((ix < n_lines)&&(!lines[ix].includes("="))){
+	    
+	    if (lines[ix].includes(":")){
+		var bits = lines[ix].split(":");
+		desc_obj.img_comments[bits[0]] = trim(bits[1]);
+	    }
+	    ix++;
+	}
+	DescriptionsArr.push(desc_obj);
+    }
+
+    var str = "var hand_descriptions = {\ntext_obj: "+JSON.stringify(DescriptionsArr, null, 2)+"\n};"
+
+    fs.writeFile(path_output_file, str, function(err) {
+	if(err) {
+            return console.log(err);
+	}
+
+	console.log("The 'hand-descriptions.js' was saved!");
+    });
+
+});
---
+++ hand-descriptions.js
@@ -0,0 +1,134 @@
+var hand_descriptions = {
+    text_obj: [
+	{
+	    "pattern_id": "139-01",
+	    "completion_date": "December 2016",
+	    "materials_used": "Fine black fibre-tip, watercolour pencils and fine paintbrush",
+	    "description": "This is my first hand-drawn pattern consistent with the intended constraints of the pattern program. The colour set (\"colour pot\") mimics a part of the Southbank Centre interior. The vertical bars are \"randomly\" placed, but dense at the edges and sparer at the centre.",
+	    "dimentions": "5.x cm × 2.x cm (check it)",
+	    "img_comments": {
+		"139-01-1600px01.jpg": "I ought to retake this photo using a flatbed scanner instead of my phone camera."
+	    }
+	},
+	{
+	    "pattern_id": "139-02",
+	    "completion_date": "December 2016",
+	    "materials_used": "Black fibre-tip & ballpoint pens; watercolour pencils and paintbrush",
+	    "description": "This pattern demonstrates the fundamental concept I intended for my patterns program. A pattern is composed of a repeating unit and proprties of that repeating unit may be varied, independently, over space. In this case, spot size, outline thickness and fill-colour are linearly swept. Can you see what direction this is in, for each?",
+	    "dimentions": "7.x cm × 7.x cm (check it)",
+	    "img_comments": {
+		"139-02-1600px02.jpg": "The unevenness of the pencil shading combined with the photo/compression quality causes a mottled effect. My program will not reproduce this easily, but to first order it should be able to reproduce patterns like this one in just a couple of minutes."
+	    }
+	},
+	{
+	    "pattern_id": "139-03",
+	    "completion_date": "February 2017",
+	    "materials_used": "Fine black fibre-tip, and black ballpoint pen. Watercolour pencils (dipped in water). Fine paintbrush",
+	    "description": "In this pattern, a radial gradient is applied to the \"rotation\" property of a square motif. The squares at the centre are shown at 0 degrees, whilst those at the edges are at 45 degrees. Other than this, a completely regular placement is used (i.e. dots at the centres of each square for a diamond pattern). I used my computer to supply a stream of random numbers, so that I didn't have to decide which of the tinier squares would be green, yellow, purple or orange. Besides rotation, two other properties of the motif follow the same radial gradient, can you see what they are (clue: one of them is a probability).",
+	    "dimentions": "This pattern took an extremely long time to finish, I started it in December '16! It was the realisation that it was probably no less interesting in its partially completed state that inspired me to take a lot of work-in-progress photos of all patterns, as I drew them.",
+	    "img_comments": {
+		"E": "",
+		"139-03-1600px01.jpg": "Each of the smaller squares is 2mm x 2mm. To fill them in, I dip the watercolour pencil in water, dry it slightly and make a point on the page. Then I spread out the pigment using a wet paintbrush.",
+		"139-03-1600px03.jpg": "Its a fine permanent marker I use to split the regions of colour into clean cells.",
+		"139-03-1600px05.jpg": "The black background is coloured in using a \"uni-ball eye\" ballpoint pen, which has a good ink-flow.",
+		"139-03-1600px07.jpg": "The camera on my Samsung phone gives a sharper photo if its LED flash is switched on, but the drawback is that this black ink has a particular sheen which causes a lot of reflection."
+	    }
+	},
+	{
+	    "pattern_id": "139-04",
+	    "completion_date": "January 2017",
+	    "materials_used": "0.5mm mechanical pencil, watercolour pencil, fine coloured fibre-tips, black pen.",
+	    "description": "This is my first of my compositions to combine the two fundamental elements of this series of patterns: regularly placed motifs and randomly placed motifs. Where small regularly placed circles have their size varied according to some underlying design, we move to the technique of Halftone (see wikipedia article https://en.wikipedia.org/wiki/Halftone). Colours........(tbc)",
+	    "dimentions": "X.x cm × X.x cm",
+	    "img_comments": {}
+	},
+	{
+	    "pattern_id": "146-01",
+	    "completion_date": "",
+	    "materials_used": "",
+	    "description": "",
+	    "dimentions": "",
+	    "img_comments": {}
+	},
+	{
+	    "pattern_id": "146-02",
+	    "completion_date": "",
+	    "materials_used": "",
+	    "description": "",
+	    "dimentions": "",
+	    "img_comments": {}
+	},
+	{
+	    "pattern_id": "146-03",
+	    "completion_date": "",
+	    "materials_used": "",
+	    "description": "",
+	    "dimentions": "",
+	    "img_comments": {}
+	},
+	{
+	    "pattern_id": "146-04",
+	    "completion_date": "",
+	    "materials_used": "",
+	    "description": "",
+	    "dimentions": "",
+	    "img_comments": {}
+	},
+	{
+	    "pattern_id": "",
+	    "completion_date": "",
+	    "materials_used": "",
+	    "description": "",
+	    "dimentions": "",
+	    "img_comments": {}
+	},
+	{
+	    "pattern_id": "",
+	    "completion_date": "",
+	    "materials_used": "",
+	    "description": "",
+	    "dimentions": "",
+	    "img_comments": {}
+	},
+	{
+	    "pattern_id": "",
+	    "completion_date": "",
+	    "materials_used": "",
+	    "description": "",
+	    "dimentions": "",
+	    "img_comments": {}
+	},
+	{
+	    "pattern_id": "",
+	    "completion_date": "",
+	    "materials_used": "",
+	    "description": "",
+	    "dimentions": "",
+	    "img_comments": {}
+	},
+	{
+	    "pattern_id": "",
+	    "completion_date": "",
+	    "materials_used": "",
+	    "description": "",
+	    "dimentions": "",
+	    "img_comments": {}
+	},
+	{
+	    "pattern_id": "",
+	    "completion_date": "",
+	    "materials_used": "",
+	    "description": "",
+	    "dimentions": "",
+	    "img_comments": {}
+	},
+	{
+	    "pattern_id": "",
+	    "completion_date": "",
+	    "materials_used": "",
+	    "description": "",
+	    "dimentions": "",
+	    "img_comments": {}
+	}
+    ]
+};
