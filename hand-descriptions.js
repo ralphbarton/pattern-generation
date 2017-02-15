30721f03daa7f5a4771dd1470f4266781f2d18c0
Ralph Barton <ralph_barton_@hotmail.com>
2017-02-15 17:09:02 +0000

    Examples gallery: updated thumbnail file-set and text-descriptions
     - dimentions added for each picture, in the text dimentions
     - a more comprehensive set of thumbnails
     - this commit represents autogenerated JSON coming from txt file process and folder contents reading

--- hand-descriptions.js
+++ hand-descriptions.js
@@ -5,7 +5,7 @@ text_obj: [
     "completion_date": "December 2016",
     "materials_used": "Fine black fibre-tip, watercolour pencils and fine paintbrush",
     "description": "This is my first hand-drawn pattern consistent with the intended constraints of the pattern program. The colour set (\"colour pot\") mimics a part of the Southbank Centre interior. The vertical bars are \"randomly\" placed, but dense at the edges and sparer at the centre.",
-    "dimentions": "5.x cm × 2.x cm (check it)",
+    "dimentions": "9.7 cm × 4.3 cm",
     "img_comments": {
       "139-01-1600px01.jpg": "I ought to retake this photo using a flatbed scanner instead of my phone camera."
     }
@@ -15,7 +15,7 @@ text_obj: [
     "completion_date": "December 2016",
     "materials_used": "Black fibre-tip & ballpoint pens; watercolour pencils and paintbrush",
     "description": "This pattern demonstrates the fundamental concept I intended for my patterns program. A pattern is composed of a repeating unit and proprties of that repeating unit may be varied, independently, over space. In this case, spot size, outline thickness and fill-colour are linearly swept. Can you see what direction this is in, for each?",
-    "dimentions": "7.x cm × 7.x cm (check it)",
+    "dimentions": "9.7 cm × 9.4 cm",
     "img_comments": {
       "139-02-1600px02.jpg": "The unevenness of the pencil shading combined with the photo/compression quality causes a mottled effect. My program will not reproduce this easily, but to first order it should be able to reproduce patterns like this one in just a couple of minutes."
     }
@@ -24,8 +24,8 @@ text_obj: [
     "pattern_id": "139-03",
     "completion_date": "February 2017",
     "materials_used": "Fine black fibre-tip, and black ballpoint pen. Watercolour pencils (dipped in water). Fine paintbrush",
-    "description": "In this pattern, a radial gradient is applied to the \"rotation\" property of a square motif. The squares at the centre are shown at 0 degrees, whilst those at the edges are at 45 degrees. Other than this, a completely regular placement is used (i.e. dots at the centres of each square for a diamond pattern). I used my computer to supply a stream of random numbers, so that I didn't have to decide which of the tinier squares would be green, yellow, purple or orange. Besides rotation, two other properties of the motif follow the same radial gradient, can you see what they are (clue: one of them is a probability).\n<br>This pattern took an extremely long time to finish, I started it in December '16! It was the realisation that it was probably no less interesting in its partially completed state that inspired me to take a lot of work-in-progress photos of all patterns, as I drew them.",
-    "dimentions": "X.x cm × X.x cm",
+    "description": "In this pattern, a radial gradient is applied to the \"rotation\" property of a square motif. The squares at the centre are shown at 0 degrees, whilst those at the edges are at 45 degrees. Other than this, a completely regular placement is used (i.e. dots at the centres of each square for a diamond pattern). I used my computer to supply a stream of random numbers, so that I didn't have to decide which of the tinier squares would be green, yellow, purple or orange. Besides rotation, two other properties of the motif follow the same radial gradient, can you see what they are (clue: one of them is a probability).\n<br>\n<br>This pattern took an extremely long time to finish, I started it in December '16! It was the realisation that it was probably no less interesting in its partially completed state that inspired me to take a lot of work-in-progress photos of all patterns, as I drew them.",
+    "dimentions": "9.8 cm × 14.1 cm",
     "img_comments": {
       "139-03-1600px01.jpg": "Each of the smaller squares is 2mm x 2mm. To fill them in, I dip the watercolour pencil in water, dry it slightly and make a point on the page. Then I spread out the pigment using a wet paintbrush.",
       "139-03-1600px03.jpg": "Its a fine permanent marker I use to split the regions of colour into clean cells.",
@@ -37,8 +37,8 @@ text_obj: [
     "pattern_id": "139-04",
     "completion_date": "January 2017",
     "materials_used": "0.5mm mechanical pencil, watercolour pencil, fine coloured fibre-tips, black pen.",
-    "description": "This is my first of my compositions to combine the two fundamental elements of this series of patterns: regularly placed motifs and randomly placed motifs. Where small regularly placed circles have their size varied according to some underlying design, we move to the technique of Halftone (see wikipedia article https://en.wikipedia.org/wiki/Halftone). Colours........(tbc)",
-    "dimentions": "X.x cm × X.x cm",
+    "description": "This is my first of my compositions to combine the two fundamental elements of this series of patterns: regularly placed motifs and randomly placed motifs. Where small regularly placed circles have their size varied according to some underlying design, we move to the technique of Halftone (see <a href=\"https://en.wikipedia.org/wiki/Halftone\">wikipedia article</a>). Colours........(tbc)",
+    "dimentions": "20.1 cm × 14.1 cm",
     "img_comments": {}
   },
   {
@@ -46,7 +46,7 @@ text_obj: [
     "completion_date": "",
     "materials_used": "",
     "description": "",
-    "dimentions": "",
+    "dimentions": "16.0 cm × 3.8 cm",
     "img_comments": {}
   },
   {
@@ -54,7 +54,7 @@ text_obj: [
     "completion_date": "",
     "materials_used": "",
     "description": "",
-    "dimentions": "",
+    "dimentions": "16.0 cm × 5.8 cm",
     "img_comments": {}
   },
   {
@@ -62,7 +62,7 @@ text_obj: [
     "completion_date": "",
     "materials_used": "",
     "description": "",
-    "dimentions": "",
+    "dimentions": "7.9 cm × 8.1 cm",
     "img_comments": {}
   },
   {
@@ -70,63 +70,79 @@ text_obj: [
     "completion_date": "",
     "materials_used": "",
     "description": "",
-    "dimentions": "",
+    "dimentions": "16.0 cm × 10.0 cm",
     "img_comments": {}
   },
   {
-    "pattern_id": "",
-    "completion_date": "",
+    "pattern_id": "146-05",
+    "completion_date": "not yet complete (feb '17)",
     "materials_used": "",
     "description": "",
-    "dimentions": "",
+    "dimentions": "7.7 cm × 8.1 cm",
     "img_comments": {}
   },
   {
-    "pattern_id": "",
+    "pattern_id": "152-01",
     "completion_date": "",
     "materials_used": "",
     "description": "",
-    "dimentions": "",
+    "dimentions": "7.8 cm × 9.6 cm",
     "img_comments": {}
   },
   {
-    "pattern_id": "",
+    "pattern_id": "152-02",
     "completion_date": "",
     "materials_used": "",
     "description": "",
-    "dimentions": "",
+    "dimentions": "6.6 cm × 29.0 cm",
     "img_comments": {}
   },
   {
-    "pattern_id": "",
+    "pattern_id": "152-03",
     "completion_date": "",
     "materials_used": "",
     "description": "",
-    "dimentions": "",
+    "dimentions": "5.1 cm × 9.6 cm",
     "img_comments": {}
   },
   {
-    "pattern_id": "",
+    "pattern_id": "153-01",
     "completion_date": "",
     "materials_used": "",
     "description": "",
-    "dimentions": "",
+    "dimentions": "6.5 cm × 6.1 cm",
     "img_comments": {}
   },
   {
-    "pattern_id": "",
+    "pattern_id": "153-02",
     "completion_date": "",
     "materials_used": "",
     "description": "",
-    "dimentions": "",
+    "dimentions": "6.5 cm × 6.1 cm",
     "img_comments": {}
   },
   {
-    "pattern_id": "",
+    "pattern_id": "153-03",
     "completion_date": "",
     "materials_used": "",
     "description": "",
-    "dimentions": "",
+    "dimentions": "6.5 cm × 6.1 cm",
+    "img_comments": {}
+  },
+  {
+    "pattern_id": "153-04",
+    "completion_date": "",
+    "materials_used": "",
+    "description": "",
+    "dimentions": "20.1 cm × 6.0 cm",
+    "img_comments": {}
+  },
+  {
+    "pattern_id": "153-05",
+    "completion_date": "doesn't exist yet (Feb '17)",
+    "materials_used": "",
+    "description": "",
+    "dimentions": "20.3 cm × 16.1 cm",
     "img_comments": {}
   }
 ]
