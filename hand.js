4d8882e9b1f4583cecdb79dca9a8abea03d07f93
Ralph Barton <ralph_barton_@hotmail.com>
2017-02-19 00:28:29 +0000

    Examples Gallery: Added feature to zoom in and pan the image, with various sub-features:
     - when panning, the drag-region is correctly bounded by an invisible DIV, so that image cannot be "dragged outside viewport"
     - upon zoom command, image is centered
     - as things stand, zoom-level is fixed...
     - Grey box whilst in zoomed state
     - Zoomed state is reset by image change, pattern change or starting slideshow

--- hand.js
+++ hand.js
@@ -93,6 +93,7 @@ var hand = {
 	$("#tabs-8 #play-pause #pp-icon").click(function(){
 
 	    if($(this).attr("class") == "play"){
+		hand.set_panning_zoom(false);//unzoom image
 		hand.slideshow();
 		$("#tabs-8 #play-pause #pp-icon").removeClass("play").addClass("pause");
 	    }else{
@@ -128,30 +129,91 @@ var hand = {
 	    }, 3200);
 	});
 
+
+
 	$("#tabs-8 img#magnify-icon").on("click", function(){
 
-	    //add the "zooming" class
-	    $("#tabs-8 #img-section")
-		.toggleClass("normal")
-		.toggleClass("zooming")
-
-	    if( $("#tabs-8 #img-section").hasClass("zooming") ){
-
-		$("#tabs-8 #img-section img.pattern-photo")
-		    .draggable();
-
-	    }else{
-
-		$("#tabs-8 #img-section img.pattern-photo")
-		    .draggable( "destroy" )
-		    .attr("style","");//remove inline styles not removed by the "destroy" method above...
-
-	    }
-
+	    hand.set_panning_zoom();
 
 	});
 
     },
+
+
+    set_panning_zoom: function(switch_on){
+
+
+	if(switch_on === undefined){//then toggle whatever it is...
+
+	    $("#tabs-8 #img-section")
+		.toggleClass("normal")
+		.toggleClass("zooming")
+	    switch_on = $("#tabs-8 #img-section").hasClass("zooming");
+	}else{
+
+	    $("#tabs-8 #img-section")
+		.toggleClass("normal", !switch_on)
+		.toggleClass("zooming", switch_on)
+	    
+
+	}
+
+	if(switch_on){
+	    //Changes for the panning zoom.
+
+	    var pho_W = parseInt($(".pattern-photo").css("width"));
+	    var pho_H = parseInt($(".pattern-photo").css("height"));
+
+	    var win_W = parseInt($("#img-section").css("width"));
+	    var win_H = parseInt($("#img-section").css("height"));
+
+	    var pan_box_Left = win_W - pho_W;
+	    var pan_box_Top = win_H - pho_H;
+	    var pan_box_Width = 2*pho_W - win_W;
+	    var pan_box_Height = 2*pho_H - win_H;
+	    var img_central_Left = (win_W - pho_W)/2;
+	    var img_central_Top = (win_H - pho_H)/2;
+
+	    //handle case of image smaller than view-window. X and Y directions are independant
+	    if(pho_W < win_W){
+		var pan_box_Left = (win_W - pho_W)/2;
+		var pan_box_Width = pho_W;
+	    }
+	    if(pho_H < win_H){
+		var pan_box_Top = (win_H - pho_H)/2;
+		var pan_box_Height = pho_H;
+	    }
+
+	    $("#panning-img-container")
+		.css("left", pan_box_Left)
+		.css("top", pan_box_Top)
+		.css("width", pan_box_Width)
+		.css("height", pan_box_Height)
+
+	    $("#tabs-8 #img-section img.pattern-photo")
+		.draggable({
+		    containment: $("#panning-img-container")
+		})
+		.css("left", img_central_Left)
+		.css("top", img_central_Top);
+
+	}else{
+
+	    //Changes to remove the panning zoom.
+
+	    try{// use TRY statement because destroy will throw an error if it is not a dragable anyway
+		$("#tabs-8 #img-section img.pattern-photo")
+		    .draggable( "destroy" )
+		    .attr("style","");//remove inline styles not removed by the "destroy" method above...
+	    }catch(err){
+		// nevermind
+	    }
+
+	}
+
+    },
+
+
 
     slideshow_fast: false,
     slideshow_timeout_id: undefined,
@@ -288,7 +350,9 @@ var hand = {
     show_pattern: function(pattern_id){
 
 	//cancel any ongoing slideshow if moving between patterns...
+	this.set_panning_zoom(false);//unzoom image
 	this.stop_slideshow();
+
 
 	//get the obj for Textual details...
 	var Arr = hand_descriptions.text_obj;
@@ -332,6 +396,7 @@ var hand = {
 			.toggleClass("selected", i==final_i)
 			.click(function(){
 			    //cancel any ongoing slideshow if user selects photo
+			    hand.set_panning_zoom(false);//unzoom image
 			    hand.stop_slideshow();
 
 			    $(".button.pho").removeClass("selected");
