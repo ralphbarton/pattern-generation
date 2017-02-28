a379fffdd5641ef7837c913c03644b492b88614e
Ralph Barton <ralph_barton_@hotmail.com>
2017-02-28 11:38:51 +0000

    Examples gallery: changes to descriptive text

--- hand-descriptions.js
+++ hand-descriptions.js
@@ -4,7 +4,7 @@ text_obj: [
     "pattern_id": "139-01",
     "completion_date": "December 2016",
     "materials_used": "Fine black fibre-tip, watercolour pencils and fine paintbrush",
-    "description": "This is my first hand-drawn pattern consistent with the intended constraints of the pattern program. The colour set (\"colour pot\") mimics a part of the Southbank Centre interior. The vertical bars are \"randomly\" placed, but dense at the edges and sparer at the centre.",
+    "description": "This is my first hand-drawn pattern following the (intended) rules of the pattern program. The colour set (\"colour pot\") copies a part of the Southbank Centre interior. The vertical bars are \"randomly\" placed, but dense at the edges and sparer at the centre.",
     "dimentions": "9.7 cm × 4.3 cm",
     "img_comments": {
       "139-01-1600px01.jpg": "I ought to retake this photo using a flatbed scanner instead of my phone camera."
@@ -13,31 +13,32 @@ text_obj: [
   {
     "pattern_id": "139-02",
     "completion_date": "December 2016",
-    "materials_used": "Black fibre-tip & ballpoint pens; watercolour pencils and paintbrush",
-    "description": "This pattern demonstrates the fundamental concept I intended for my patterns program. A pattern is composed of a repeating unit and proprties of that repeating unit may be varied, independently, over space. In this case, spot size, outline thickness and fill-colour are linearly swept. Can you see what direction this is in, for each?",
+    "materials_used": "Black fibre-tip & ballpoint pens; watercolour pencils (& paintbrush)",
+    "description": "This pattern demonstrates the basic principle of the patterns program. A pattern is composed of a repeating \"motif\". The various proprties of that motif may be varied - each independently - over space. In this case, spot size, outline thickness and fill-colour are linearly swept. Can you see what direction this is in, for each?",
     "dimentions": "9.7 cm × 9.4 cm",
     "img_comments": {
-      "139-02-1600px02.jpg": "The unevenness of the pencil shading combined with the photo/compression quality causes a mottled effect. My program will not reproduce this easily, but to first order it should be able to reproduce patterns like this one in just a couple of minutes."
+      "139-02-1600px02.jpg": "Solid colour areas have a mottled effect, some combination of uneven pencil shading and photo compression quality. On the other hand, art created with my program be completely regular. It should be able to reproduce simple patterns like this one in just a couple of minutes."
     }
   },
   {
     "pattern_id": "139-03",
     "completion_date": "February 2017",
-    "materials_used": "Fine black fibre-tip, and black ballpoint pen. Watercolour pencils (dipped in water). Fine paintbrush",
-    "description": "In this pattern, a radial gradient is applied to the \"rotation\" property of a square motif. The squares at the centre are shown at 0 degrees, whilst those at the edges are at 45 degrees. Other than this, a completely regular placement is used (i.e. dots at the centres of each square for a diamond pattern). I used my computer to supply a stream of random numbers, so that I didn't have to decide which of the tinier squares would be green, yellow, purple or orange. Besides rotation, two other properties of the motif follow the same radial gradient, can you see what they are (clue: one of them is a probability).\n<br>\n<br>This pattern took an extremely long time to finish, I started it in December '16! It was the realisation that it was probably no less interesting in its partially completed state that inspired me to take a lot of work-in-progress photos of all patterns, as I drew them.",
+    "materials_used": "Fine black fibre-tip & ballpoint pens. Watercolour pencils (dipped in water) + brush",
+    "description": "In this pattern a gradient is applied to the \"rotation\" property of a square motif. Motifs at the centre are shown at 0 degrees, whilst those at the edges are at 45 degrees. Actual positioning of the motif-centres uses a regular square grid at 45 degrees. I used my computer to supply a stream of random numbers to decide for me which of the tinier squares would be green, yellow, purple or orange. Besides rotation, two other properties of the motif follow the same radial gradient, can you see what they are (clue: one of them is a probability).\n<br>\n<br>This pattern took a long time to finish, I started it in December '16! When I realised that it was perhaps no less interesting left partially finished, I was inspired me to take a lot of in-progress photos of the patterns I was drawing. This itself provided the source material for this images gallery.",
     "dimentions": "9.8 cm × 14.1 cm",
     "img_comments": {
       "139-03-1600px01.jpg": "Each of the smaller squares is 2mm x 2mm. To fill them in, I dip the watercolour pencil in water, dry it slightly and make a point on the page. Then I spread out the pigment using a wet paintbrush.",
       "139-03-1600px03.jpg": "Its a fine permanent marker I use to split the regions of colour into clean cells.",
       "139-03-1600px05.jpg": "The black background is coloured in using a \"uni-ball eye\" ballpoint pen, which has a good ink-flow.",
-      "139-03-1600px07.jpg": "The camera on my Samsung phone gives a sharper photo if its LED flash is switched on, but the drawback is that this black ink has a particular sheen which causes a lot of reflection."
+      "139-03-1600px07.jpg": "The camera on my Samsung phone gives a sharper photo if its LED flash is switched on, but the drawback is that this black ink has a particular sheen which causes a lot of reflection.",
+      "139-03-1600px28.jpg": "Still plan to use a scan to get a really good quality version of the final image"
     }
   },
   {
     "pattern_id": "139-04",
     "completion_date": "January 2017",
     "materials_used": "0.5mm mechanical pencil, watercolour pencil, fine coloured fibre-tips, black pen.",
-    "description": "This is my first of my compositions to combine the two fundamental elements of this series of patterns: regularly placed motifs and randomly placed motifs. Where small regularly placed circles have their size varied according to some underlying design, we move to the technique of Halftone (see <a href=\"https://en.wikipedia.org/wiki/Halftone\">wikipedia article</a>). Colours........(tbc)",
+    "description": "This is my first of my compositions to combine regularly placed motifs with randomly placed motifs. Regular and random placement of motifs can be done according to some underlying design or density map. This is one of the core ideas of my program.\n<br>\n<br>Where a grid of dots have varing size which reveals some underlying design, this is the technique of Halftone (see <a href=\"https://en.wikipedia.org/wiki/Halftone\">wikipedia article</a>), common in printing.\n<br>\n<br>Colours........(tbc)",
     "dimentions": "20.1 cm × 14.1 cm",
     "img_comments": {}
   },
