ade83a1b86a25cd72f103dc0b2b589b3db214fc7
Ralph Barton <ralph_barton_@hotmail.com>
2017-03-01 22:30:44 +0000

    Gallery: timeout logic started for showing magnifier tool popup. This needs some careful thought, and then debugging

--- gallery-zoom.js
+++ gallery-zoom.js
@@ -1,6 +1,47 @@
 var gallery_zoom = {
 
+    magnif_timeout_id: undefined,
     init: function(){
+
+	var t_ani = 400;
+	var t_brief = 1000;
+	var t_long = 10000;
+
+	var set_magnif_show_time = function(t_showfor){
+
+	    clearTimeout(gallery_zoom.magnif_timeout_id);
+
+	    gallery_zoom.magnif_timeout_id = setTimeout(function(){
+		$("#magnify-popup").animate({opacity: 0}, t_ani);
+		gallery_zoom.magnif_timeout_id = undefined;
+	    }, t_showfor);
+	};
+
+
+	// respond to clicking within the image space...
+	$("#tabs-8 #img-section").on("click", function(){
+	    if(gallery_zoom.magnif_timeout_id == undefined){
+		console.log("img click");
+		$("#magnify-popup").animate({opacity: 0.9}, t_ani);
+		set_magnif_show_time(t_brief);//disappear soon (1 second)
+	    }else{
+		console.log("img click (ignored)");
+	    }
+	});
+
+	//respond to clicking a magnifier button
+	$("#magnify-popup .buttons > div").on("click", function(){
+	    set_magnif_show_time(t_long);//disappear after longer time (10 second)
+	});
+
+	//respond to clicking a magnifier button
+	$("#magnify-popup .close").on("click", function(){
+	    $("#magnify-popup").animate({opacity: 0}, t_ani);
+	    gallery_zoom.magnif_timeout_id = "holdoff";//prevent this click from causing it to show again
+	    set_magnif_show_time(t_brief);//this will re-enable the click functionality in 1 second.
+	});
+
+
 
 	var opac_med = 0.6;
 	var opac_low = 0.85;
@@ -15,13 +56,6 @@ var gallery_zoom = {
 	    }
 	    mouseenter_counter++;
 	});
-
-
-	//
-	$("#tabs-8 #img-section").on("click", function(){
-	    console.log("click");
-	});
-
 
 	//do it here rather than using css :hover (which conficts with js-managed styling responsiveness)
 	$("#tabs-8 img#magnify-icon").on("mouseenter", function(){
