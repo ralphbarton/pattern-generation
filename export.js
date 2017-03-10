02b68d39601a0b84db4d5d54662cd1760719406c
Ralph Barton <ralph_barton_@hotmail.com>
2017-03-10 20:55:23 +0000

    Options: created colour-pot import via JSON feature

--- export.js
+++ export.js
@@ -2,11 +2,16 @@ var export_pat = {
 
     init: function(){
 	
-	$("#cpanel-main-tabs #ui-id-8").click(function(){
+	$("#cpanel-main-tabs #ui-id-7").click(function(){
 	    $("#json-text").val(
 		JSON.stringify(DM.ColourPotArray, null, 2)
 	    );
 	});
+
+	$("#json-load").click(function(){
+	    DM.ColourPotArray = JSON.parse($("#json-text").val());
+	});
+
     }
 
 };
