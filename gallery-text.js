131d61b50c095ffd34ca323928133355635aa9d5
Ralph Barton <ralph_barton_@hotmail.com>
2017-03-23 15:20:11 +0000

    Gallery: update to descriptive text of my Examples patterns

--- gallery-text.js
+++ gallery-text.js
@@ -14,7 +14,7 @@ text_obj: [
     "pattern_id": "139-02",
     "completion_date": "December 2016",
     "materials_used": "Black fibre-tip & ballpoint pens; watercolour pencils (& paintbrush)",
-    "description": "This pattern demonstrates the basic principle of the patterns program. A pattern is composed of a repeating \"motif\". The various proprties of that motif may be varied - each independently - over space. In this case, spot size, outline thickness and fill-colour are linearly swept. Can you see what direction this is in, for each?",
+    "description": "This pattern demonstrates the basic principle of the patterns program. A pattern is composed of a repeating \"motif\". The various proprties of that motif may be varied - each independently - over space. In this case, spot size, outline thickness and fill-colour are linearly swept. Can you tell what the direction-of-change is, for each of these attributes?",
     "dimentions": "9.7 cm × 9.4 cm",
     "img_comments": {
       "139-02-1600px02.jpg": "Solid colour areas have a mottled effect, some combination of uneven pencil shading and photo compression quality. On the other hand, art created with my program be completely regular. It should be able to reproduce simple patterns like this one in just a couple of minutes."
@@ -24,7 +24,7 @@ text_obj: [
     "pattern_id": "139-03",
     "completion_date": "February 2017",
     "materials_used": "Fine black fibre-tip & ballpoint pens. Watercolour pencils (dipped in water) + brush",
-    "description": "In this pattern a gradient is applied to the \"rotation\" property of a square motif. Motifs at the centre are shown at 0 degrees, whilst those at the edges are at 45 degrees. Actual positioning of the motif-centres uses a regular square grid at 45 degrees. I used my computer to supply a stream of random numbers to decide for me which of the tinier squares would be green, yellow, purple or orange. Besides rotation, two other properties of the motif follow the same radial gradient, can you see what they are (clue: one of them is a probability).\n<br>\n<br>This pattern took a long time to finish, I started it in December '16! When I realised that it was perhaps no less interesting left partially finished, I was inspired me to take a lot of in-progress photos of the patterns I was drawing. This itself provided the source material for this images gallery.",
+    "description": "In this pattern a gradient is applied to the \"rotation\" property of a square motif. Motifs at the centre are shown at 0 degrees, whilst those at the edges are at 45 degrees. Actual positioning of the motif-centres uses a regular square grid at 45 degrees. I used my computer to supply a stream of random numbers to decide for me which of the tinier squares would be green, yellow, purple or orange. Besides rotation, two other properties of the motif follow the same radial gradient, can you see what they are (clue: one of them is a probability).\n<br>\n<br>This pattern took a long time to finish, I started it in December '16! When I realised that it was perhaps no less interesting left partially complete, I was inspired to take a lot of in-progress photos of all of the patterns I was drawing. This then provided the source material for which I created this images gallery.",
     "dimentions": "9.8 cm × 14.1 cm",
     "img_comments": {
       "139-03-1600px01.jpg": "Each of the smaller squares is 2mm x 2mm. To fill them in, I dip the watercolour pencil in water, dry it slightly and make a point on the page. Then I spread out the pigment using a wet paintbrush.",
@@ -38,41 +38,56 @@ text_obj: [
     "pattern_id": "139-04",
     "completion_date": "January 2017",
     "materials_used": "0.5mm mechanical pencil, watercolour pencil, fine coloured fibre-tips, black pen.",
-    "description": "This is my first of my compositions to combine regularly placed motifs with randomly placed motifs. Regular and random placement of motifs can be done according to some underlying design or density map. This is one of the core ideas of my program.\n<br>\n<br>Where a grid of dots have varing size which reveals some underlying design, this is the technique of Halftone (see <a href=\"https://en.wikipedia.org/wiki/Halftone\">wikipedia article</a>), common in printing.\n<br>\n<br>Colours........(tbc)",
+    "description": "This is my first of my compositions to combine regularly placed motifs with randomly placed motifs. Regular and random placement of motifs can be done according to some underlying low-detail design (intensity / density distribution). This is one of the core ideas of my program.\n<br>\n<br>Where a grid of dots have varing size which reveals some underlying design, this is the technique of Halftone (see <a href=\"https://en.wikipedia.org/wiki/Halftone\">wikipedia article</a>). Halftone has its origin in printing, but is now used in a huge variety of modern product branding designs.\n<br>\n<br>All of these patterns are very exacting to produce by hand, and have two phases: a fine pencil drawing following by colouring in which is the more fun bit. Just as the rigid arrangement of circles in the halftone contrasts against the style of arragement of the bubbles, I have chosen bright primary colours that juxtapose a more muted, organic palette.\n<br>\n<br>The browns, greys etc. for the bubbles were randomly picked by my computer from a continuous spectrum which I specified. The blue, red and yellow of the halftone were again picked by my computer, using a discrete probability distribution with p=1/3 for each.",
     "dimentions": "20.1 cm × 14.1 cm",
-    "img_comments": {}
+    "img_comments": {
+      "139-04-1600px02.jpg": "I used a ruler to set out the diamond-grid, and added some a",
+      "139-04-1600px04.jpg": "all of the fine-pencil lines are rubbed out (but remain visible) before I add the colour",
+      "139-04-1600px07.jpg": "outlines in fine coloured felt-tip adds definition & consistency to the halftone dots.",
+      "139-04-1600px10.jpg": "In the very final photo I finish off all outlines. Do you think the black outlines on the bubbles are necessary?"
+    }
   },
   {
     "pattern_id": "146-01",
-    "completion_date": "",
-    "materials_used": "",
-    "description": "",
+    "completion_date": "January 2017",
+    "materials_used": "mechanical pencil, watercolour pencil (mixed on separate palette), on top of fine fibre-tips",
+    "description": "This image is inspired by memories of the interiors of the bath houses in Budapest, and the effects that can be achieved when glazing ceramics.\n<br>\n<br>Take a look at the \"colour-pots\" section of the website. I think concept in the tool is particularly suitable for evoking on computer the subtle variation in colour between a batch of tiles all painted with the same glaze, as they come out of the kiln.",
     "dimentions": "16.0 cm × 3.8 cm",
-    "img_comments": {}
+    "img_comments": {
+      "146-01-1600px01.jpg": "I did not initally set about creating a grid of hexagons in the most obvious way!"
+    }
   },
   {
     "pattern_id": "146-02",
-    "completion_date": "",
-    "materials_used": "",
-    "description": "",
+    "completion_date": "January 2017",
+    "materials_used": "mechanical pencil, watercolour pencil, fine coloured fibre-tips",
+    "description": "The motif is a rectangle with rounded corners, it is tiled at an angle and filled with a colour spanning from lemon yellow to crimson.\n<br>\n<br>Whilst colours of the rectangles are random, something else is happening with the bluish spot in the centre of each.",
     "dimentions": "16.0 cm × 5.8 cm",
-    "img_comments": {}
+    "img_comments": {
+      "146-02-1600px02.jpg": "oops. The picture got splashed, and now needs touching up with acrylic paints."
+    }
   },
   {
     "pattern_id": "146-03",
-    "completion_date": "",
-    "materials_used": "",
+    "completion_date": "January 2017",
+    "materials_used": "mechanical pencil, watercolour pencil (mixed on separate palette), black fibre-tip.",
     "description": "",
     "dimentions": "7.9 cm × 8.1 cm",
-    "img_comments": {}
+    "img_comments": {
+      "146-03-1600px01.jpg": "a bounding box defines each overlapping cirle",
+      "146-03-1600px02.jpg": "overlapping bounding boxes create quite a tight grid",
+      "146-03-1600px03.jpg": "maybe I should have stopped here... The black background seems to make the subtly different colours all look washed-out."
+    }
   },
   {
     "pattern_id": "146-04",
-    "completion_date": "",
-    "materials_used": "",
-    "description": "",
+    "completion_date": "January 2017",
+    "materials_used": "0.5mm mechanical pencil, watercolour pencil, fine coloured fibre-tips.",
+    "description": "The concept of this composition ......",
     "dimentions": "16.0 cm × 10.0 cm",
-    "img_comments": {}
+    "img_comments": {
+      "146-04-1600px01.jpg": "lighting is poor"
+    }
   },
   {
     "pattern_id": "146-05",
