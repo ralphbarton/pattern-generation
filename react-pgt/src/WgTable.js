9f33222c5cfa300ac99af7f29a25ff3928816760
Ralph Barton <ralph_barton_@hotmail.com>
2017-05-20 03:05:42 +0100

    React Rewrite: WgTable.js is a "Functional Component" (render method only; no state)

--- react-pgt/src/WgTable.js
+++ react-pgt/src/WgTable.js
@@ -1,67 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './WgTable.css';
 
 // for colour pot preview rendering...
 import CpotCellBlock from './CpotCellBlock';
 
 
-class WgTable extends Component {
-    
-    render() {
-	return (
-	    <table className="WgTable">
-	      <thead>
-		<tr>
-		  {
-		      this.props.columns.map( (column, index) => {
-			  return (
-			      <th
-				 key={index}
-				 className={"col-"+(index+1)} >	
-				{column.name}
-			      </th>);
-		      })
-		  }
-	    </tr>
-		</thead>
-		<tbody>
-		{
-		    this.props.cpotArray.map( (cpot, index) => {
-			return (
-			    <tr className={index === this.props.selectedRowIndex ? "selected" : null}
-				key={cpot.uid}
-				onClick={() => {
-				    if(index !== this.props.selectedRowIndex){
-					this.props.onRowSelectedChange(index);
-				    }
-				  }
+function WgTable(props) {
+    return (
+	<table className="WgTable">
+	  <thead>
+	    <tr>
+	      {
+		  props.columns.map( (column, index) => {
+		      return (
+			  <th
+			     key={index}
+			     className={"col-"+(index+1)} >	
+			    {column.name}
+			  </th>);
+		  })
+	      }
+	</tr>
+	    </thead>
+	    <tbody>
+	    {
+		props.cpotArray.map( (cpot, index) => {
+		    return (
+			<tr className={index === props.selectedRowIndex ? "selected" : null}
+			    key={cpot.uid}
+			    onClick={() => {
+				if(index !== props.selectedRowIndex){
+				    props.onRowSelectedChange(index);
+				}
 			      }
-			      >
-			      <td className="col-1">
-				{index+1}
-			      </td>
-			      <td className="col-2">
-				<input className="blue-cell"
-				       value={cpot.description} 
-				       onChange={ event =>{ this.props.onCpotNameChange(index, event.target.value) }}
-				  />
-				  
-			      </td>
-			      <td className="col-3">
-				<CpotCellBlock
-				   cpot={cpot}
-				   nX={8}
-				   nY={2}
-				   />
-			      </td>
-			    </tr>);
-		    })
-		}
-	    </tbody>
-		</table>
-	);
-    }
+			  }
+			  >
+			  <td className="col-1">
+			    {index+1}
+			  </td>
+			  <td className="col-2">
+			    <input className="blue-cell"
+				   value={cpot.description} 
+				   onChange={ event =>{ props.onCpotNameChange(index, event.target.value); }}
+			      />
+			      
+			  </td>
+			  <td className="col-3">
+			    <CpotCellBlock
+			       cpot={cpot}
+			       nX={8}
+			       nY={2}
+			       />
+			  </td>
+			</tr>);
+		})
+	    }
+	</tbody>
+	    </table>
+    );
 }
+
 
 
 export default WgTable;
