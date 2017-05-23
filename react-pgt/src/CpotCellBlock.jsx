7a909bb74c0728548ff83cc37149b4806d919b46
Ralph Barton <ralph_barton_@hotmail.com>
2017-05-23 01:24:00 +0100

    CpotCellBlock changed to PureComponent (doesn't rerender on very state/props change).  - It is probably the least pure of my components, all of which so far are pure, but this small tweak brings about exactly the desired speedup.

--- react-pgt/src/CpotCellBlock.jsx
+++ react-pgt/src/CpotCellBlock.jsx
@@ -3,28 +3,32 @@ import './CpotCellBlock.css';
 
 import cpot_util from './CpotUtil';
 
-function CpotCellBlock(props) {
-    
-    const makeColour = ()=>{return cpot_util.DrawFromColourPot(props.cpot);};
-    const tbodyClasses = "chequer "+ (props.chequerSize==="normal"?"":"tiny");
-    return (
-	<table className="CpotCellBlock"><tbody className={tbodyClasses}>{
-		Array(props.nY).fill(null).map( (el,i) => {
-		    return ( <tr key={i}>
-			     {
-				 Array(props.nX).fill(null).map( (el,j) => {
-				     return (
-					 <td
-					    key={j}
-					    style={{backgroundColor: makeColour()}}
-					    />
-				     );
-				 })}	
-			     </tr>
-			   );
-		})}
-	</tbody></table>
-    );
+class CpotCellBlock extends React.PureComponent {
+
+    render() {
+	
+	const makeColour = ()=>{return cpot_util.DrawFromColourPot(this.props.cpot);};
+	const tbodyClasses = "chequer "+ (this.props.chequerSize==="normal"?"":"tiny");
+
+	return (
+	    <table className="CpotCellBlock"><tbody className={tbodyClasses}>{
+		    Array(this.props.nY).fill(null).map( (el,i) => {
+			return ( <tr key={i}>
+				 {
+				     Array(this.props.nX).fill(null).map( (el,j) => {
+					 return (
+					     <td
+						key={j}
+						style={{backgroundColor: makeColour()}}
+						/>
+					 );
+				     })}	
+				 </tr>
+			       );
+		    })}
+	    </tbody></table>
+	);
+    }
 }
 
 
