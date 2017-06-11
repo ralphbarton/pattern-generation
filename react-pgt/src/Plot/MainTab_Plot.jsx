07085cb73d26c8fcb592d38ddd4521a51c0096cc
Ralph Barton <ralph_barton_@hotmail.com>
2017-06-11 18:26:45 +0100

    Density Plots: ongoing UI layout work: Zoom & Rotate section layed out  - continued use of Flexbox (and width-controlled inline-block where this is enough)  - work on a WgSpecialButton component. This is for buttons that are an icon rather than text.  - Other rejigging of the styling of the 'Plots' Tab

--- react-pgt/src/Plot/MainTab_Plot.jsx
+++ react-pgt/src/Plot/MainTab_Plot.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 // generic project widgets
 import WgTable from '../Wg/WgTable';
 import WgButton from '../Wg/WgButton';
+import WgSpecialButton from '../Wg/WgSpecialButton';
 import WgBoxie from '../Wg/WgBoxie';
 import WgMutexActionLink from '../Wg/WgMutexActionLink';
 import WgActionLink from '../Wg/WgActionLink';
@@ -136,7 +137,144 @@ class MainTab_Plot extends React.PureComponent {
 
 		{/* Middle: Zoom Controls */}
 		<WgBoxie className="zoomRotate" name="Zoom & Rotate" boxieStyle={"small"}>
-		  Zoom controls...
+
+		  <div className="sectionLinks1">
+		    <WgActionLink
+		       name={"Reset Zoom"}
+		       onClick={null}
+		       enabled={true}
+		       />
+		    <WgActionLink
+		       name={"Square Axes"}
+		       onClick={null}
+		       enabled={true}
+		       />
+		    <WgActionLink
+		       name={"More"}
+		       onClick={null}
+		       enabled={true}
+		       />
+		  </div>
+
+		  
+		  <div className="sectionButtons">
+
+		    <div className="btns-zoom">
+		      <div>Zoom:</div>		      
+		      <WgSpecialButton
+			 className="mediumSquare"
+			 iconName="Plus"
+			 onClick={null}
+			 />
+		      <WgSpecialButton
+			 className="mediumSquare"
+			 iconName="Minus"
+			 onClick={null}
+			 />
+		    </div>
+
+		    <div className="btns-rotate">
+		      <div>Rotate:</div>
+		      <WgSpecialButton
+			 className="mediumSquare"
+			 iconName="arrowClockwiseRing"
+			 onClick={null}
+			 />
+		      <WgSpecialButton
+			 className="mediumSquare"
+			 iconName="arrowAnticlockwiseRing"
+			 onClick={null}
+			 />
+		    </div>
+
+		    <div className="btns-translate">
+		      <div>Translate:</div>
+		      <WgSpecialButton
+			 className="mediumSquare"
+			 iconName="arrowUp"
+			 onClick={null}
+			 />
+		      <WgSpecialButton
+			 className="mediumSquare"
+			 iconName="arrowDown"
+			 onClick={null}
+			 />
+		      <WgSpecialButton
+			 className="mediumSquare"
+			 iconName="arrowLeft"
+			 onClick={null}
+			 />
+		      <WgSpecialButton
+			 className="mediumSquare"
+			 iconName="arrowRight"
+			 onClick={null}
+			 />
+		    </div>
+
+		  </div>
+
+		  
+		  <div className="sectionLinks2">
+
+		    <WgMutexActionLink
+		       name="Mouse Zoom:"
+		       className="mouseZoom"
+		       initalEnabledArray={[false, false]}
+		       actions={[
+			   {
+			       name: "On"
+			   },{
+			       name: "Off"
+			   }
+		       ]}
+		       />
+
+		    <WgMutexActionLink
+		       name="Aspect ratio:"
+		       className="aspectRatio"
+		       initalEnabledArray={[false, false]}
+		       actions={[
+			   {
+			       name: "lock"
+			   },{
+			       name: "unlock"
+			   }
+		       ]}
+		       />
+
+		    <WgMutexActionLink
+		       name="Zoom:"
+		       className="zoomXonlyYonly"
+		       initalEnabledArray={[false, false, false]}
+		       actions={[
+			   {
+			       name: "x,y"
+			   },{
+			       name: "x only"
+			   },{
+			       name: "y only"
+			   }
+		       ]}
+		       />
+
+		    <WgMutexActionLink
+		       name="Steps:"
+		       className="stepsSML"
+		       initalEnabledArray={[false, false, false]}
+		       actions={[
+			   {
+			       name: "S"
+			   },{
+			       name: "M"
+			   },{
+			       name: "L"
+			   }
+		       ]}
+		       />
+
+		    
+		  </div>
+
 		</WgBoxie>
 
 		{/* Bottom: Preview options */}
--- react-pgt/src/Wg/WgSpecialButton.jsx
+++ react-pgt/src/Wg/WgSpecialButton.jsx
@@ -1,17 +1,60 @@
 import React from 'react';
-import WgButton from 'react';
+import WgButton from './WgButton';
+
+import ArrowLeftImage from './WgSpecialButton-asset/arrow-left-s.png';
+import ArrowUpImage from './WgSpecialButton-asset/arrow-up-s.png';
+import ArrowRightImage from './WgSpecialButton-asset/arrow-right-s.png';
+import ArrowDownImage from './WgSpecialButton-asset/arrow-down-s.png';
+import ArrowClockwiseImage from './WgSpecialButton-asset/rotate-clockwise-s.png';
+import ArrowAnticlockwiseImage from './WgSpecialButton-asset/rotate-anticlockwise-s.png';
+
 
 function WgSpecialButton(props) {
 
-    const buttonClasses = "none";
+
+    const jsxIMG = (Img, Cl) => {return <img className={Cl} src={Img} alt={"icon-img-" + Cl} />;};
+    const getBtnjsx = (iconString) => {
+	switch (iconString){
+
+	case "arrowLeft":
+	    return jsxIMG(ArrowLeftImage, "arrow");
+
+	case "arrowUp":
+	    return jsxIMG(ArrowUpImage, "arrow");
+
+	case "arrowRight":
+	    return jsxIMG(ArrowRightImage, "arrow");
+
+	case "arrowDown":
+	    return jsxIMG(ArrowDownImage, "arrow");
+
+	case "Plus":
+	    return <span className="symbolPlus">+</span>;
+
+	case "Minus":
+	    return <span className="symbolMinus">–</span>;
+
+	case "arrowClockwiseRing":
+	    return jsxIMG(ArrowClockwiseImage, "ringArrow");
+
+	case "arrowAnticlockwiseRing":
+	    return jsxIMG(ArrowAnticlockwiseImage, "ringArrow");
+	    
+	default:
+	    return <span>A</span>;
+	    
+	}
+    };
+
+    
     return (
-	<WgButton
-	   className={buttonClasses}{/* custom styling... */}
-	   enabled={props.enabled}
-	   onClick={props.onClick}
-	   name={"Spec"}{/*this HTML content gets injected inside button...*/}
+	<WgButton className={"WgSpecialButton " + props.className + " " + props.iconName}
+		  enabled={props.enabled}
+		  onClick={props.onClick}
+		  name={getBtnjsx(props.iconName)}
 	   />
     );
+
 }
 
 export default WgSpecialButton;
