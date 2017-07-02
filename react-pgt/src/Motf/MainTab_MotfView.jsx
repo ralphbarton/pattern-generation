d2f32c37e1fc9f62387ffcb5d48c5aa4b7315f54
Ralph Barton <ralph_barton_@hotmail.com>
2017-07-03 00:16:43 +0100

    Motifs: basic UI layout of tab ported over.

--- react-pgt/src/Motf/MainTab_MotfView.jsx
+++ react-pgt/src/Motf/MainTab_MotfView.jsx
@@ -4,10 +4,12 @@ import React from 'react';
 
 import WgTable from '../Wg/WgTable';
 import WgButton from '../Wg/WgButton';
+import WgBoxie from '../Wg/WgBoxie';
+
 /*
-import WgTabbedBoxie from '../Wg/WgTabbedBoxie';
-import WgActionLink from '../Wg/WgActionLink';
-*/
+ import WgTabbedBoxie from '../Wg/WgTabbedBoxie';
+ import WgActionLink from '../Wg/WgActionLink';
+ */
 
 
 class MainTab_MotfView extends React.PureComponent {
@@ -77,53 +79,86 @@ class MainTab_MotfView extends React.PureComponent {
     render(){
 
 	if(this.props.UI.selectedRowIndex === undefined){return null;}
-//	const Motf_i = this.props.PGTobjArray[this.props.UI.selectedRowIndex];
+	//	const Motf_i = this.props.PGTobjArray[this.props.UI.selectedRowIndex];
 
 	return (
 
 	    <div className="MainTab_MotfView">
-	      	      
-	      {/* 1.  Table, and accompanying controls (i.e. buttons beneath) */}
-	      <div className="tableWithButtonsZone">
-		<WgTable
-		   selectedRowIndex={this.props.UI.selectedRowIndex}
-		   onRowSelectedChange={(i)=>{this.props.fn.handleRowSelectedChange(i);}}
-		  rowRenderingData={this.props.PGTobjArray}
-		  columnsRendering={this.motf_WgTableColumns()}
-		  rowClassingFn={this.rowClassingFn}
-		  />
+	      
+	      {/* Column 1:  Table, buttons beneath, and 1× boxie) */}
+	      <div className="column1">
+		<div className="tableWithButtonsZone">
+		  <WgTable
+		     selectedRowIndex={this.props.UI.selectedRowIndex}
+		     onRowSelectedChange={(i)=>{this.props.fn.handleRowSelectedChange(i);}}
+		    rowRenderingData={this.props.PGTobjArray}
+		    columnsRendering={this.motf_WgTableColumns()}
+		    rowClassingFn={this.rowClassingFn}
+		    />
 
-		  <div className="mainButtons">		  
+		    <div className="mainButtons">		  
+		      
+		      <WgButton
+			 name="Delete"
+			 buttonStyle={"small"}
+			 enabled={true}
+			 />
+		      <WgButton
+			 name="Add"
+			 buttonStyle={"small"}
+			 enabled={true}
+			 />
+		      <WgButton
+			 name="Dupl."
+			 buttonStyle={"small"}
+			 enabled={true}
+			 />
+		      <WgButton
+			 name="Edit"
+			 buttonStyle={"small"}
+			 enabled={true}
+			 />
+
+		    </div>
 		    
-		    <WgButton
-		       name="Delete"
-		       buttonStyle={"small"}
-		       enabled={true}
-		       />
-		    <WgButton
-		       name="Add"
-		       buttonStyle={"small"}
-		       enabled={true}
-		       />
-		    <WgButton
-		       name="Dupl."
-		       buttonStyle={"small"}
-		       enabled={true}
-		       />
-		    <WgButton
-		       name="Edit"
-		       buttonStyle={"small"}
-		       enabled={true}
-		       />
+		</div>
 
-		  </div>
-		  
+		
+
+		{/* Column 2:  3× boxie) */}
+		<WgBoxie className="stats" name="stats" boxieStyle={"small"}>
+		  In Motif: How many shapes? How many colours? 
+		</WgBoxie>
+
+	      </div>
+	      <div className="column2">
+		
+		<WgBoxie className="cpots" name="Colour Pots used" boxieStyle={"small"}>
+		  Autumn leaves [re-randomise]
+		</WgBoxie>
+
+		<WgBoxie className="inputParams" name="Input Parameters" boxieStyle={"small"}>
+		  K01 - animate<br/>
+		  K02 - animate<br/>
+		  K03 - animate 
+		</WgBoxie>
+
+		<WgBoxie className="randomParams" name="Random Parameters" boxieStyle={"small"}>
+		  R01 - animate<br/>
+		  R02 - animate<br/>
+		  R03 - animate 
+		</WgBoxie>
+
 	      </div>
 
-
-
+	      {/* Column 1:  the preview...) */}
+	      <div className="column3">
+		<div className="preview">
+		  <div className="title">My Motif 2</div>
+		  <svg></svg>
+		</div>
+	      </div>
 	      
-	      {/* 3.  Just add more stuff here... */}
 	      
 	    </div>
 
