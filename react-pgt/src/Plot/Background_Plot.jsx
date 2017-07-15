20c6ec4b28703a4a869098695aa4f150da53fa14
Ralph Barton <ralph_barton_@hotmail.com>
2017-07-15 12:24:10 +0100

    Plots: 'shouldComponentUpdate()' method of 'Background_Plot.jsx' a bit smarter  - only rerenders the plot when UI-state actually affecting it has changed (positive selection)  - resolution change does not trigger a coarse resolution fast re-render; only the specified resolution render.

--- react-pgt/src/Plot/Background_Plot.jsx
+++ react-pgt/src/Plot/Background_Plot.jsx
@@ -13,6 +13,9 @@ class Background_Plot extends React.PureComponent {
 
     constructor() {
 	super();
+	this.state = {
+	    resolutionChangeOnly: false
+	};
 	
 	Plot_RenderManager.init({
 	    onRenderComplete: this.handleRenderComplete.bind(this),
@@ -51,8 +54,32 @@ class Background_Plot extends React.PureComponent {
 	console.log("Fast render complete in: ", ((new Date())-this.t));
     }
 
+    componentWillReceiveProps(nextProps){
+	//setState does not impact upon this.state until sometime later...
+	this.setState({
+	    // assume that if the resolution changes, it is the only thing that has changed.
+	    // this is fair since it is a user updatable value, and the user cannot literally update 2 things in one event
+	    resolutionChangeOnly: this.props.plotUIState.plotResolution !== nextProps.plotUIState.plotResolution
+	});
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+
+	// only changes to some specific props should bring about re-execution of the render function
+	const c1 = this.props.plotUIState.selectionUid      !== nextProps.plotUIState.selectionUid;
+	const c2 = this.props.plotUIState.colouringFunction !== nextProps.plotUIState.colouringFunction;
+	const c3 = this.props.plotUIState.previewActive     !== nextProps.plotUIState.previewActive;
+	const c4 = this.props.plotUIState.plotResolution    !== nextProps.plotUIState.plotResolution;
+
+	console.log("this.state.resolutionChangeOnly", this.state.resolutionChangeOnly);
+	return c1 || c2 || c3 || c4;
+    }
+
     componentDidUpdate(){
-	this.handleFastCanvasRender();
+	//no fast-update when only the resolution changes.
+	if(!this.state.resolutionChangeOnly){
+	    this.handleFastCanvasRender();
+	}
     }
 
     componentDidMount(){
@@ -66,6 +93,11 @@ class Background_Plot extends React.PureComponent {
 	const plotUIState = this.props.plotUIState;
 	if(!plotUIState.previewActive){return null;}
 
+
+	//TODO:
+
+	//some effort is required to limit occurances of rerender to when acually necessary
+	
 	console.log("<Background_Plot> render() called", plotUIState);
 
 	//this update is always important.
