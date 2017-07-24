b7fedfc65f9cfbd09919422d8732888201684d2e
Ralph Barton <ralph_barton_@hotmail.com>
2017-07-24 17:38:19 +0100

    Plots: Preview Options: some layout work to incorporate a box for "Timings" details.  - dummy text (and no text formatting) in this commit.

--- react-pgt/src/Plot/Plot_Section_PreviewOptions.jsx
+++ react-pgt/src/Plot/Plot_Section_PreviewOptions.jsx
@@ -15,107 +15,120 @@ class Plot_Section_PreviewOptions extends React.PureComponent {
 	return (
 	    <div className="PreviewOptions">
 
-	      <div className="inputContainer plotResolution">
-		Plot Resolution:
-		<WgSmartInput
-		   className="plain-cell s"
-		   value={this.props.UI.plotResolution}
-		   dataUnit="pixels"
-		   max={160}
-		   onChange={(value)=>{
-		       console.log("call 'handleUIStateChange' with", value);
-		  handleUIStateChange("plotResolution", value);}}
-		  />
-	      </div>
 
-	      <WgMutexActionLink
-		 name="Scale:"
-		 className="overlayAxesScale"
-		 equityTestingForEnabled={{
-		     currentValue: this.props.UI.overlayAxesScale,
-		     representedValuesArray: [true, false]
-		 }}
-		 actions={[
-		     {
-			 name: "show",
-			 cb: handleUIStateChange.bind(null, "overlayAxesScale", true)
-		     },{
-			 name: "hide",
-			 cb: handleUIStateChange.bind(null, "overlayAxesScale", false)
-		     }
-		 ]}
-		 />
-	      
-	      
-	      <WgMutexActionLink
-		 name="Colouring:"
-		 className="colouringFunction"
-		 equityTestingForEnabled={{
-		     currentValue: this.props.UI.colouringFunction,
-		     representedValuesArray: [1, 2]
-		 }}
-		 actions={[
-		     {
-			 name: "greyscale",
-			 cb: handleUIStateChange.bind(null, "colouringFunction", 1)
-		     },{
-			 name: "heatmap",
-			 cb: handleUIStateChange.bind(null, "colouringFunction", 2)
-		     }
-		 ]}
-		 />
+	      <div className="timingContoursStrip">	      
 
-	      <div className="mainHideShow">
-
-		<WgActionLink
-		   name={"Hide Preview"}
-		   onClick={handleUIStateChange.bind(null, "previewActive", false)}
-		   enabled={this.props.UI.previewActive}
-		   />
-
+		<div className="Timing">
+		  Thumbs: 100ms <br/>
+		  Fast Render: 30ms<br/>
+		  Final Render: 2560 ms
+		</div>
 		
-		<WgButton
-		   name="Plot Preview"
-		   onClick={handleUIStateChange.bind(null, "previewActive", true)}
-		   enabled={this.props.validFormulaSelected}
-		   />
+		<div className="Contours">
+
+		  <WgMutexActionLink
+		     name="Contours:"
+		     className="showContours"
+		     equityTestingForEnabled={{
+			 currentValue: this.props.UI.showContours,
+			 representedValuesArray: [true, false]
+		     }}
+		     actions={[
+			 {
+			     name: "show",
+			     cb: handleUIStateChange.bind(null, "showContours", true)
+			 },{
+			     name: "hide",
+			     cb: handleUIStateChange.bind(null, "showContours", false)
+			 }
+		     ]}
+		     />
+
+		  <div className="inputContainer">
+		    Quantity:&nbsp;
+		    <WgSmartInput
+		       className="plain-cell s"
+		       value={this.props.UI.quantityContours}
+		       dataUnit="dimentionless"
+		       step={1}
+		       max={40}
+		       onChange={(value)=>{handleUIStateChange("quantityContours", value);}}
+		      />
+		  </div>
+		</div>
+		
 	      </div>
 
-	      <div className="innerbox">
+
+	      <div className="unboxedPadder">	      
+		<div className="inputContainer plotResolution">
+		  Plot Resolution:
+		  <WgSmartInput
+		     className="plain-cell s"
+		     value={this.props.UI.plotResolution}
+		     dataUnit="pixels"
+		     max={160}
+		     onChange={(value)=>{
+			 console.log("call 'handleUIStateChange' with", value);
+		    handleUIStateChange("plotResolution", value);}}
+		    />
+		</div>
 
 		<WgMutexActionLink
-		   name="Contours:"
-		   className="showContours"
+		   name="Scale:"
+		   className="overlayAxesScale"
 		   equityTestingForEnabled={{
-		       currentValue: this.props.UI.showContours,
+		       currentValue: this.props.UI.overlayAxesScale,
 		       representedValuesArray: [true, false]
 		   }}
 		   actions={[
 		       {
 			   name: "show",
-			   cb: handleUIStateChange.bind(null, "showContours", true)
+			   cb: handleUIStateChange.bind(null, "overlayAxesScale", true)
 		       },{
 			   name: "hide",
-			   cb: handleUIStateChange.bind(null, "showContours", false)
+			   cb: handleUIStateChange.bind(null, "overlayAxesScale", false)
+		       }
+		   ]}
+		   />
+		
+		
+		<WgMutexActionLink
+		   name="Colouring:"
+		   className="colouringFunction"
+		   equityTestingForEnabled={{
+		       currentValue: this.props.UI.colouringFunction,
+		       representedValuesArray: [1, 2]
+		   }}
+		   actions={[
+		       {
+			   name: "greyscale",
+			   cb: handleUIStateChange.bind(null, "colouringFunction", 1)
+		       },{
+			   name: "heatmap",
+			   cb: handleUIStateChange.bind(null, "colouringFunction", 2)
 		       }
 		   ]}
 		   />
 
-		<div className="inputContainer">
-		  Quantity:&nbsp;
-		  <WgSmartInput
-		     className="plain-cell s"
-		     value={this.props.UI.quantityContours}
-		     dataUnit="dimentionless"
-		     step={1}
-		     max={40}
-		     onChange={(value)=>{handleUIStateChange("quantityContours", value);}}
-		    />
+		<div className="mainHideShow">
+
+		  <WgActionLink
+		     name={"Hide Preview"}
+		     onClick={handleUIStateChange.bind(null, "previewActive", false)}
+		     enabled={this.props.UI.previewActive}
+		     />
+
+		  
+		  <WgButton
+		     name="Plot Preview"
+		     onClick={handleUIStateChange.bind(null, "previewActive", true)}
+		     enabled={this.props.validFormulaSelected}
+		     />
 		</div>
 
 	      </div>
-
-	    </div>
+	    </div> 
 
 	);
     }
