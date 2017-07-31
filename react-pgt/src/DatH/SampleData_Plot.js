e5e460416a23e9a12a66d2a1d894fce8305be7e3
Ralph Barton <ralph_barton_@hotmail.com>
2017-07-31 16:37:17 +0100

    Plots: added an additional interesting Plot as SampleData  - this one uses a pre-set Rendering scale as well as a pre-set section...

--- react-pgt/src/DatH/SampleData_Plot.js
+++ react-pgt/src/DatH/SampleData_Plot.js
@@ -54,6 +54,22 @@ const PlotSampleData = {
 		rotation: 0
 	    },
 	    lastRenderScale: {}
+	},
+	{
+	    formula: "z*log( (z-0.7)*(z+0.7*i)*(z+0.2*(i+1.3)) )^4",
+	    uid: 4,
+	    section: {
+		xOffset: 0.71,
+		yOffset: -0.064,
+		xZoom: 0.094,
+		yZoom: 0.094,
+		rotation: 0
+	    },
+	    autoScale: false,
+	    lastRenderScale: {
+		Lo: -25,
+		Hi: 35
+	    }
 	}
     ]
 };
