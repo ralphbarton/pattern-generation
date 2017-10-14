f83cf0546481df241c3a7766d31ceec64dbab401
Ralph Barton <ralph_barton_@hotmail.com>
2017-10-14 18:51:54 +0100

    Background Drawings: work started on scrollable "Drawing Picker" interface  - layout / styling of a scrollable list of drawings, and accompanying text  - Clicking a particular drawing from the choices selects that one for display  - New file "ImgFiles_util.js" to extract details, aspect ratio etc. of a Drawing

--- react-pgt/src/Back/Background_Drawing.jsx
+++ react-pgt/src/Back/Background_Drawing.jsx
@@ -2,41 +2,105 @@ import React from 'react';
 var _ = require('lodash');
 
 import gallery_files from './plain-js/gallery-files.js';
-import gallery_text  from './plain-js/gallery-text.js';
+import ImgFiles_util  from './plain-js/ImgFiles_util.js';
 
-
+import numeral from 'numeral';// for aspect ratios
 
 class Background_Drawing extends React.PureComponent {
-    
-    render() {
 
-	const path = process.env.PUBLIC_URL + "/img1600/"; //path_fullsize
+    constructor() {
+	super();
+	this.state = {
+	    selection: null//"139-01"
+	};
 
 	//copy-pasted...
-	var dict_fullsize = {};
+	this.dict_fullsize = {};
+	const TS = this;
 	gallery_files.fullsize_img.forEach(function(filename) {
 
 	    var img_code = filename.slice(0,6);
-	    if(dict_fullsize[img_code] === undefined){
-		dict_fullsize[img_code] = [];
+	    if(TS.dict_fullsize[img_code] === undefined){
+		TS.dict_fullsize[img_code] = [];
 	    }
-	    dict_fullsize[img_code].push(filename);
+	    TS.dict_fullsize[img_code].push(filename);
 
 	});
+	
+    }
 
+    renderChooser(){
 
-//	      <textarea>{JSON.stringify(gallery_files, null, 2)}</textarea>
-
-	const img_details = _.find(gallery_text.text_obj, {"pattern_id": "139-01"} );  
-	const aspect = img_details.dimentions.split("×").map( s => {return s.replace(/[^0-9.,]+/g, '');});
+	const path_S = process.env.PUBLIC_URL + "/thumb360/"; //path_thumb
+	const TS = this;
 	
 	return (
-	    <div className="Background_Drawing">
-	      <img src={ path+gallery_files.fullsize_img[0] } alt=""/>
-	      <textarea value={aspect[0]} readOnly={true}/>
-	      <textarea value={aspect[1]} readOnly={true}/>
+	    <div className="Background_Drawing chooser">
+	      {
+		  _.map(this.dict_fullsize, function(value, key){
+		      const aspect = ImgFiles_util.getAspect(key);
+		      const r = aspect[0]/aspect[1];
+		      const imgWidth = Math.min(360, TS.props.dims.width/2);
+		      return (
+			  <div key={key}
+			       onClick={() => {
+				   TS.setState({
+				       selection: key
+				   });
+			       }}
+			       >
+
+			    <img src={ path_S + key + "-thumb.Primary.jpg" }
+				 alt=""
+				 style={{width: imgWidth}}/ >
+			      
+			    <div className="text">
+			      <div className="name">{key}</div>
+			      <div className="date">{ImgFiles_util.getDetails(key)["completion_date"]}</div>
+			      <div className="aspect">Aspect (W:H): <span>{numeral(r).format('0.00')} : 1</span></div>
+			    </div>
+
+			  </div>
+		      );
+		  })
+	      }
 	    </div>
 	);
+    }
+
+    
+    renderDrawing(){
+
+	const path_L = process.env.PUBLIC_URL + "/img1600/";  //path_fullsize
+	//	      <textarea>{JSON.stringify(gallery_files, null, 2)}</textarea>
+
+	const imgKey = this.state.selection;
+	const ImgSet = this.dict_fullsize[imgKey];
+	
+ 	return (
+	    <div className="Background_Drawing drawing">
+	      <img src={ path_L+ImgSet[ImgSet.length-1] } alt=""/>
+	      <a onClick={() => {
+		    this.setState({
+			selection: null
+		    });
+		}}>
+		back
+	      </a>
+	    </div>
+	);
+    }
+
+    
+    render() {
+
+
+	if(this.state.selection === null){
+	    return this.renderChooser();
+	}else{
+	    return this.renderDrawing();
+	}
+
     }
     
 }
---
+++ react-pgt/src/Back/plain-js/ImgFiles_util.js
@@ -0,0 +1,19 @@
+var _ = require('lodash');
+
+import gallery_files from './gallery-files.js';
+import gallery_text  from './gallery-text.js';
+
+var ImgFiles_util = {
+
+    getDetails: function(img_identity_str){
+	return _.find(gallery_text.text_obj, {"pattern_id": img_identity_str} );  
+    },
+
+    getAspect: function(img_identity_str){
+	const img_details = this.getDetails(img_identity_str);
+	return img_details.dimentions.split("×").map( s => {return s.replace(/[^0-9.,]+/g, '');});
+    }
+
+};
+
+export {ImgFiles_util as default};
