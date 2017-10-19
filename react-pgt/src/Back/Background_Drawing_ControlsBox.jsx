c674eceb08b9cb1cc865f26dbd25fb9735316f3a
Ralph Barton <ralph_barton_@hotmail.com>
2017-10-19 01:45:36 +0100

    Background Drawings: Controls Box: work on layout, & UI states held+indicated  - more minimal non-expanded state (but now, even Drawings with very few progress images have a "more" link to access zoom feature)  - when expanded: new arrangment of Drawing's: date+dims, fast/slow slideshow, show description and magnifer  - Functionality of show/hide description: text is retrieved and put on screen  - UI state held for magnifier controls active, and for slideshow speed. Visual indicators are +/-/no icon inside magnifier, and asterisk, respectively.  - Slider shows when magnifier clicked  - When Controls box disappears on mouse off, all extra bits contract  - ported pause icon for slideshow

--- react-pgt/src/Back/Background_Drawing_ControlsBox.jsx
+++ react-pgt/src/Back/Background_Drawing_ControlsBox.jsx
@@ -1,35 +1,54 @@
 import React from 'react';
 
+import WgSlider from '../Wg/WgSlider';
+
 import ImgFiles_util  from './plain-js/ImgFiles_util.js';
 
 import Img_threeSquare from './asset/three-square.png';
 import Img_iconPlay from './asset/icon-play.png';
-import Img_iconMagnify from './asset/icon-magnify.png';
+import Img_iconPause from './asset/icon-pause.png';
+import Img_iconMagnifyPlus  from './asset/icon-magnify-plus.png';
+import Img_iconMagnifyMinus from './asset/icon-magnify-minus.png';
+import Img_iconMagnifyNone  from './asset/icon-magnify-none.png';
 
 class Background_Drawing_ControlsBox extends React.PureComponent {
 
     constructor() {
 	super();
 	this.state = {
-	    expanded: false
+	    expanded: false,
+	    description: false,
+	    magnifier: false,
+	    magnification: 100,
+	    isFast: true,
+	    playing: false
 	};
     }
     
     render() {
+	
+	
 	const imgKey = this.props.selectedImgKey;
 	const imgIdx = this.props.selectedImgIdx;
 	const N = this.props.dict_fullsize[imgKey].length;
-
+	
  	return (
 	    <div className="Background_Drawing_ControlsBox"
 		 onMouseLeave={()=>{
 		     this.setState({
-			 expanded: false
+			 expanded: false,
+			 description: false, // re-hide description
+			 magnifier: false // re-hide magnifier
 		     });
 	      }}>
-	      <div className="A">
-		<button>
-		  <img src={Img_iconPlay} alt={""} />
+	      <div className="A play">
+		<button
+		   onClick={()=>{
+		       this.setState({
+			   playing: !this.state.playing
+		       });
+		  }}>
+		  <img src={this.state.playing ? Img_iconPause : Img_iconPlay} alt={""} />
 		</button>
 	      </div>
 
@@ -38,12 +57,12 @@ class Background_Drawing_ControlsBox extends React.PureComponent {
 	      <div className="B">
 		<div className="name">{imgKey}</div>		
 
-		<div className={"actLink more" + (N<5 ? " disabled" : "")}
-						      onClick={()=>{
-							  this.setState({
-							      expanded: true
-							  });
-		     }}>
+		<div className="actLink more"
+			 onClick={()=>{
+			     this.setState({
+				 expanded: true
+			     });
+			 }}>
 		  more...
 		</div>
 
@@ -75,21 +94,57 @@ class Background_Drawing_ControlsBox extends React.PureComponent {
 		    <div className="dims">{ImgFiles_util.getDetails(imgKey)["dimentions"]}</div>
 		  </div>
 
-		  <div className={"actLink more" + (N<5 ? " disabled" : "")}
-							onClick={()=>{
-							    console.log("speed change");
+		  <div className="speed">		  
+
+		    <div>
+		      {this.state.isFast ? <span>* </span> : null}
+		      <div className="actLink"
+		           onClick={()=>{
+			     this.setState({isFast: true});
+			 }}>
+		      fast
+		      </div>
+		    </div>
+		  
+		    <div>
+		      {!this.state.isFast ? <span>* </span> : null}
+		      <div className="actLink"
+		           onClick={()=>{
+			     this.setState({isFast: false});
+			 }}>
+		      slow
+		      </div>
+		    </div>
+
+		  </div>
+
+		  <div className="actLink description"
+		       onClick={()=>{
+			   this.setState({description: !this.state.description});
 		       }}>
-		    fast slow
+		  {this.state.description?"hide":"show"} description
 		  </div>
 
 		  <div className="magnify">
-		    <img src={Img_iconMagnify}
-			 onClick={()=>{console.log("magnify click...");}}
-		      alt={""} />
+
+		  {/* 0=unmagnified, 1=Magnfied+expanded state, 2=magnified, contracted state*/}
+		  <img src={
+		             [Img_iconMagnifyPlus, Img_iconMagnifyMinus, Img_iconMagnifyNone]
+		             [this.state.magnification>100 ? (this.state.magnifier?1:2) : 0]// eslint-disable-line
+			   }
+		       onClick={()=>{
+			   const isMagnif = this.state.magnification > 100;
+			   this.setState({
+			       magnifier: !this.state.magnifier,
+			       //so no change if already magnified, but magnify controls contracted
+			       magnification: (isMagnif ? (this.state.magnifier ? 100 : this.state.magnification) : 200)
+			   });
+			   console.log("magnify click...");
+		       }}
+		       alt={""} />
 		  </div>
 
-		  <div className="actLink description">description</div>
-		</div>
+		  </div>
 		}
 	    
 
@@ -119,6 +174,28 @@ class Background_Drawing_ControlsBox extends React.PureComponent {
 		  </div>
 		  </div>
 		}
+	        {
+		  this.state.magnifier &&
+		  <WgSlider
+		    min={100}
+		    max={600}
+		    value={this.state.magnification}
+		    onChange={ v => {
+			this.setState({magnification: v});
+		    }}
+		  />
+		}
+	        {
+		    this.state.description &&
+			<div className="D">
+			<div className="description">
+			    {ImgFiles_util.getDetails(imgKey)["description"]}
+		        </div>
+			</div>			  
+		}
+
+
+	    
 	      </div>
 	);
     }
