68da94ed45927b1403a36a9993dafb3c479c0a20
Ralph Barton <ralph_barton_@hotmail.com>
2017-10-28 18:55:46 +0100

    Motif utility functions: code tidying without functional change  - "newRandomMotif()" revamped code for more lodash. Functionally equivalent.  - "Fabric_AddShape()" now does less as I have moved most of its code into "parseMotifElement()"

--- react-pgt/src/Motf/plain-js/Motf_util.js
+++ react-pgt/src/Motf/plain-js/Motf_util.js
@@ -17,26 +17,8 @@ import Motf_lists from './Motf_lists';
 var Motf_util = {
     
     newRandomMotif: function(){
-
-	//it will be 50% a circle, 50% a rectangle...
-	var isCircle = Math.random() > 0.5;
-	var Motif_Element_One = {
-	    "shape": isCircle ? "obj-ellipse" : "obj-rectangle",
-	    "left": isCircle ? 0 : 50,
-	    "top": isCircle ? 0 : 50,
-	    "fill": tinycolor.random().toHexString(),
-	    "stroke": null,
-	    "PGTuid": 0
-	};
 	
-	if(isCircle){
-	    Motif_Element_One["rx"] = 200;
-	    Motif_Element_One["ry"] = 200;	    
-	}else{
-	    Motif_Element_One["width"]  = 300;
-	    Motif_Element_One["height"] = 300;	    
-	}
-	
+	const isCircle = Math.random() > 0.5;	// 50% chance of circle; 50% chance of rectangle...
 
 	return {
 	    name: "New Motif",
@@ -45,10 +27,40 @@ var Motf_util = {
 		"random": [],
 		"CP_picks": []
 	    },
-	    Elements: [Motif_Element_One]
+	    Elements: [
+		_.assign(
+		    {
+			"shape": isCircle ? "obj-ellipse" : "obj-rectangle",
+			"left": isCircle ? 0 : 50,
+			"top": isCircle ? 0 : 50,
+			"fill": tinycolor.random().toHexString(),
+			"stroke": null,
+			"PGTuid": 0
+		    },
+		    isCircle ?
+			{
+			    "rx": 200,
+			    "ry": 200
+			} : {
+			    "width":  300,
+			    "height": 300
+			}
+
+		)
+	    ]
 	};
     },
 
+	    /*
+	      //demo code for adding applying a background pattern in Fabric JS
+
+	    fabric.util.loadImage(TextureImg, function(img) {
+		new_shape.set('fill', new fabric.Pattern({
+		    source: img,
+		    repeat: "repeat"
+		}));
+	    });
+	    */
     
     parseMotifElement: function(format, Element, options){
 
@@ -56,13 +68,54 @@ var Motf_util = {
 
 	if(format === 'fabric'){ // "properties object" for Fabric
 
-	    // for any shape
-	    
-	    // Every Fabric key (of the Element) is the string used as its DatH_Key
-	    const validKeys = ShapeDetails.ObjectFabricProperties.map( o =>{return o.DatH_Key ;} );
+	    if(ShapeDetails.fabricKey){ // Shapes for which a key "fabricKey" is non-null are 'native Fabric shapes'		
+		
+		// Every Fabric key (of the Element) is the string used as its DatH_Key
+		const validKeys = ShapeDetails.ObjectFabricProperties.map( o =>{return o.DatH_Key ;} );
 
-	    // Filter the Element object for valid props, using Lodash pick
-	    return _.pick(Element, validKeys);
+		/*
+		// Todo: somehow re-integrated this logic...
+
+		//Fabric assumes strokeWidth 1 if stroke supplied
+		stroke: (Element.strokeWidth !== undefined) ? Element.stroke : null, 
+		strokeWidth: Element.strokeWidth || null,// undefined causes problems but null OK!
+	     */
+		
+		// Filter the Element object for valid props, using Lodash pick
+		return {
+		    name: ShapeDetails.fabricKey,
+		    Properties: _.pick(Element, validKeys)
+		};
+	    
+	    }else{ // A shape for which key "fabricKey" is null a 'non-native Fabric shape' (e.g. hexagon)
+
+		/*
+		//   Hexagon...
+		var W1 = 0.5 * Element.width;
+		var W2 = 0.5 * Element.height / 0.866;
+		var Wm = Math.min(W1, W2);
+		var Wh = Wm * 0.866;
+
+		// 'macro' to generate an offset coordinate
+		var OC = function(x,y){
+		    return {x: x, y: y};
+		};
+		new_shape = new fabric.Polygon(
+		    [
+			OC(0.5*Wm, 0),//1
+			OC(1.5*Wm, 0),
+			OC(2*Wm, Wh),
+			OC(1.5*Wm, 2*Wh),//4
+			OC(0.5*Wm, 2*Wh),
+			OC(0, Wh)
+		    ], {
+			left: Element.left,
+			top: Element.top,
+			fill: Element.fill
+		    }
+		);
+		*/	    
+	    }
 
 
 	}else if(format === 'svg'){ // "properties object" for SVG
@@ -137,132 +190,19 @@ var Motf_util = {
 	    // 2. append a new SVG element accordingly
 	    d3_svg
 		.append(rendering_props.name)
-		.attr("class","some-obj")
 		.attrs( rendering_props.attrs)
 		.styles( rendering_props.styles );
 	    	    
 	});
-	
 
-	
-
-/*
-	_.forEach(Motif.Elements, function(E, key) {//E is element properties object
-
-	    // This is a hack...
-	    // in the sense that key-conversion should be handled elsewhere...
-	    if( E["strokeWidth"] ){ E["stroke-width"] = E["strokeWidth"]; }
-
-	    // further tweaking of properties format to suit the d3 requirements on object properties
-
-	    // put the origin in the middle of the Image (other ways to do this in an SVG?)
-	    // I don't think this is a final solution, below...
-	    E["x"] = E.left - (options.originZero ? 200 : 0);
-	    E["y"] = E.top  - (options.originZero ? 200 : 0);
-
-	    E["transform"] = "rotate("+(E.angle||0)+", "+E.x+", "+E.y+")";
-
-	    // ellipse is positioned by its center point (these props are for ellipse only)
-	    E["cx"] = E.x + E.rx;
-	    E["cy"] = E.y + E.ry;
-
-	    //Create an Ellipse
-	    if(E.shape === "obj-ellipse"){
-
-		
-		//an angle may not be in data passed...
-		d3_svg.append("ellipse").attr("class","some-obj")
-		// some properties can be utilised verbatim (d3/svg attrs)
-		    .attrs( _.pick(E, ["cx", "cy", "rx", "ry", "transform"]))
-		// some properties can be utilised verbatim (d3/svg styles)
-		    .styles( _.pick(E, ["fill", "stroke", "stroke-width"]) );
-		
-	    }else if(E.shape === "obj-rectangle"){
-		
-		//Create a Rectangle
-		d3_svg.append("rect").attr("class","some-obj")
-		// some properties can be utilised verbatim (d3/svg attrs)
-		    .attrs( _.pick(E, ["x", "y", "width", "height", "transform"]))
-		// some properties can be utilised verbatim (d3/svg styles)
-		    .styles( _.pick(E, ["fill", "stroke", "stroke-width"]) );
-
-	    }	    
-	    
-	});*/
-
-	
     },
 
 
     // generate Fabric object from object saved in DatH
     Fabric_AddShape: function(canvas, Element){
-
-	/*
-	  Port more of the logic here into the other function, "parseMotifElement()" ?
-	 */
-	
-	const ShapeDetails = _.find(Motf_lists.ObjectTypes, {DatH_name: Element.shape} );
-	var new_shape = undefined;
-	
-	if(ShapeDetails.fabricKey){ // test if this shape has a Fabric key stored with...
-
-	    // its a native Fabric shape
-	    const rendering_props = this.parseMotifElement("fabric", Element);
-	    new_shape = new fabric[ShapeDetails.fabricKey]( rendering_props );
-
-	    /*
-	      //demo code for adding applying a background pattern in Fabric JS
-
-	    fabric.util.loadImage(TextureImg, function(img) {
-		new_shape.set('fill', new fabric.Pattern({
-		    source: img,
-		    repeat: "repeat"
-		}));
-	    });
-	    */
-
-//	    new_shape.set('top', Math.random()*200);
-	    
-	    /*
-	      // I have not re-integrated this logic...
-	      stroke: (Element.strokeWidth !== undefined) ? Element.stroke : null, //Fabric assumes strokeWidth 1 if stroke supplied
-	      strokeWidth: Element.strokeWidth || null,// undefined causes problems but null OK!
-	     */
-	    
-	}else{
-	    // not a Native shape for fabric JS...
-
-/*
-
-  //   Hexagon...
-	    var W1 = 0.5 * Element.width;
-	    var W2 = 0.5 * Element.height / 0.866;
-	    var Wm = Math.min(W1, W2);
-	    var Wh = Wm * 0.866;
-
-	    // 'macro' to generate an offset coordinate
-	    var OC = function(x,y){
-		return {x: x, y: y};
-	    };
-	    new_shape = new fabric.Polygon(
-		[
-		    OC(0.5*Wm, 0),//1
-		    OC(1.5*Wm, 0),
-		    OC(2*Wm, Wh),
-		    OC(1.5*Wm, 2*Wh),//4
-		    OC(0.5*Wm, 2*Wh),
-		    OC(0, Wh)
-		], {
-		    left: Element.left,
-		    top: Element.top,
-		    fill: Element.fill
-		}
-	    );
-*/	    
-	}
-
-	//Not done by the Fabric Contructor: set UID according to value provided.
-	new_shape.PGTuid = Element.PGTuid;	
+	const rendering_props = this.parseMotifElement("fabric", Element);
+	const new_shape = new fabric[ rendering_props.name ]( rendering_props.Properties );
+	new_shape.PGTuid = Element.PGTuid; //Not (obviously) done by the Fabric Contructor: setting UID
 	canvas.add(new_shape);
     },
 
