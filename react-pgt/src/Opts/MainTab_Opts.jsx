a6d9849e0438e4c81eecf49e267b749e90f9f7da
Ralph Barton <ralph_barton_@hotmail.com>
2017-11-11 14:59:06 +0000

    Options Tab: lots of new UI (placeholder only)  - JSON export  - explitly set pane dimentions  - solid backgound colour ( - a note about 'further options')

--- react-pgt/src/Opts/MainTab_Opts.jsx
+++ react-pgt/src/Opts/MainTab_Opts.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import WgBoxie from '../Wg/WgBoxie';
+import WgSmartInput from '../Wg/WgSmartInput';
+import {WgMutexActionLink, WgMut2WayActionLink} from '../Wg/WgMutexActionLink';
 
 class MainTab_Opts extends React.PureComponent {
 
@@ -25,34 +27,123 @@ class MainTab_Opts extends React.PureComponent {
 	const noOp = ()=>{};
 	
 	return (
-	    <div className={"MainTab_Opts"}>
+	    <div className="MainTab_Opts">
 
-	    <WgBoxie className="screenUsage" name="Screen Usage" >
+	      <div className="column1">
+		
+		<WgBoxie className="screenUsage" name="Screen Usage" >
+		  
+		  Divide screen into:
+		  <label onClick={setUI("mode", 0)}>
+		    <input type="radio" onChange={noOp} checked={UI.mode === 0}/>
+		    Single Pane
+		  </label>
 
-	      Dividing screen into:
-	      <label onClick={setUI("mode", 0)}>
-		<input type="radio" onChange={noOp} checked={UI.mode === 0}/>
-		Single Pane
-	      </label>
+		  <label onClick={setUI("mode", 1)}>
+		    <input type="radio" onChange={noOp} checked={UI.mode === 1} />
+		    Two panes (vertical)
+		  </label>
 
-	      <label onClick={setUI("mode", 1)}>
-		<input type="radio" onChange={noOp} checked={UI.mode === 1} />
-		Two panes (vertical)
-	      </label>
+		  <label onClick={setUI("mode", 2)}>
+		    <input type="radio" onChange={noOp} checked={UI.mode === 2} />
+		    Two panes (horizontal)
+		  </label>
 
-	      <label onClick={setUI("mode", 2)}>
-		<input type="radio" onChange={noOp} checked={UI.mode === 2} />
-		Two panes (horizontal)
-	      </label>
+		  <label onClick={setUI("mode", 3)}>
+		    <input type="radio" onChange={noOp} checked={UI.mode === 3} />
+		    Four panes
+		  </label>
 
-	      <label onClick={setUI("mode", 3)}>
-		<input type="radio" onChange={noOp} checked={UI.mode === 3} />
-		Four panes
-	      </label>
+		  <div className="fixedDimentions">
 
-	      </WgBoxie>
+		    <WgMut2WayActionLink
+		       name="Set fixed pane dimentions"
+		       variableName="isFixedDimentions"
+		       value={/*UI.fixedDimentions*/ null}
+		       hofCB={/*setUI*/ ()=>{}}/>		  
+
+
+		      <div className="WHInputs">
+			<span className="widthInput">
+			  
+			  Width:&nbsp;
+			  <WgSmartInput
+			     className="plain-cell s"
+			     value={null}
+			     dataUnit="dimentionless"
+			     step={1}
+			     onChange={(value)=>{}}
+			    />
+			</span>
+
+			<span className="heightInput">
+			  Height:&nbsp;
+			  <WgSmartInput
+			     className="plain-cell s"
+			     value={null}
+			     dataUnit="dimentionless"
+			     step={1}
+			     onChange={(value)=>{}}
+			    />
+			    
+			</span>
+		      </div>
+		  </div>
+
+		</WgBoxie>
+
+
+		<WgBoxie className="solidBackground" name="Solid Background" >
+		  ** colour picker here... **
+		</WgBoxie>
+
+	      </div>
 	      
+
+	      
+	      <div className="column2">
+
+		<WgBoxie className="jsonExport" name="JSON export" >
+		  
+		  <WgMutexActionLink
+		     name="Show data held for:"
+		     className="intervalUnit"
+		     equityTestingForEnabled={{
+			 currentValue: 2,
+			 representedValuesArray: [0, 1, 2, 3, 4]
+		     }}
+		     actions={[
+			 {
+			     name: "cpots",
+			     cb: ()=>{}
+			 },{
+			     name: "motfs",
+			     cb: ()=>{}
+			 },{
+			     name: "grids",
+			     cb: ()=>{}
+			 },{
+			     name: "plots",
+			     cb: ()=>{}
+			 },{
+			     name: "patts",
+			     cb: ()=>{}
+			 }
+		    ]}
+		    />
+
+		    <textarea value={"text content here json json etc..."} readOnly={true}/>
+
+		</WgBoxie>
+
+		<WgBoxie className="more" name="Further Options..." >
+		  lots more options in the pipeline. So the layout of this tab will need to be tightened up a bit.
+		</WgBoxie>
+		  
+		
+	      </div>
 	    </div>
+
 	);
     }
 }
